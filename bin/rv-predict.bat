b6a5185595f77430c4a559cd884cf78286c4ed80
Traian Florin Serbanuta <traian.serbanuta@gmail.com>
2014-05-21 14:02:47 +0300

    Additional changes due to merge.

--- bin/rv-predict.bat
+++ bin/rv-predict.bat
@@ -3,4 +3,4 @@ SET RV_ENGINE_BIN=%~dp0..\lib\rv-predict-engine.jar
 
 REM predict
 echo "predicting races"
-java -Xmx32g -cp %RV_ENGINE_BIN% NewRVPredict %*
+java -Xmx32g -cp %RV_ENGINE_BIN% rvpredict.engine.main.NewRVPredict %*

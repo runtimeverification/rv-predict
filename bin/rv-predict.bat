2566a0218ca763b58f08c65d6e8a33d4c8f4af61
Traian Florin Serbanuta <traian.serbanuta@gmail.com>
2014-07-16 16:23:07 +0300

    Updated scripts for windows

--- bin/rv-predict.bat
+++ bin/rv-predict.bat
@@ -1,5 +1,1 @@
-@echo off
-SET RV_ENGINE_BIN=%~dp0..\lib\rv-predict-engine.jar
-
-REM predict
-java -Xmx32g -cp "%RV_ENGINE_BIN%" rvpredict.engine.main.Main %*
+@java -Xmx32g -cp "%~dp0..\lib\rv-predict-engine.jar" rvpredict.engine.main.Main %*

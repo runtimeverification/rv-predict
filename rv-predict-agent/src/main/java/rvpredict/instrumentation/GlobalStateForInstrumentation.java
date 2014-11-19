0b204c9143e7004f24358a4cf8a0d91f5112bed0
Yilong Li <yli147@illinois.edu>
2014-11-19 15:30:57 -0600

    removed dead method

--- rv-predict-agent/src/main/java/rvpredict/instrumentation/GlobalStateForInstrumentation.java
+++ rv-predict-agent/src/main/java/rvpredict/instrumentation/GlobalStateForInstrumentation.java
@@ -2,15 +2,10 @@ package rvpredict.instrumentation;
 
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import com.google.common.collect.Sets;
-
-import rvpredict.config.Config;
-import rvpredict.logging.DBEngine;
-import rvpredict.logging.RecordRT;
 
 public class GlobalStateForInstrumentation {
     public static GlobalStateForInstrumentation instance = new GlobalStateForInstrumentation();
@@ -43,53 +38,6 @@ public class GlobalStateForInstrumentation {
 
     public void setSharedVariables(HashSet<String> locs) {
         this.sharedVariables = locs;
-    }
-
-    public void saveMetaData(DBEngine db) {
-        if (!Config.instance.commandLine.agentOnlySharing)
-            RecordRT.saveMetaData(db, GlobalStateForInstrumentation.instance
-            );
-        else {
-            // show arrayId
-            HashSet<Integer> sharedArrayIds = new HashSet<Integer>();
-            for (Integer sid : RecordRT.sharedArrayIds) {
-                HashSet<Integer> ids = RecordRT.arrayIdsMap.get(sid);
-                sharedArrayIds.addAll(ids);
-            }
-
-            sharedVariables = new HashSet<String>();
-            // show variableId
-            for (Map.Entry<String, Integer> entry : variableIdMap.entrySet()) {
-                Integer id = entry.getValue();
-                String var = entry.getKey();
-                if (RecordRT.sharedVariableIds.contains(id))
-                    sharedVariables.add(var);
-
-            }
-
-            sharedArrayLocations = new HashSet<String>();
-
-            for (Integer id : arrayIdMap.keySet()) {
-                String var = arrayIdMap.get(id);
-                if (sharedArrayIds.contains(id))
-                    sharedArrayLocations.add(var);
-            }
-
-            if (Config.instance.verbose) {
-                int size_var = variableIdMap.entrySet().size();
-                int size_array = arrayIdMap.entrySet().size();
-
-                double svar_percent = size_var == 0 ? 0 : ((double) RecordRT.sharedVariableIds
-                        .size() / variableIdMap.entrySet().size());
-                double sarray_percent = size_array == 0 ? 0
-                        : ((double) sharedArrayIds.size() / arrayIdMap.entrySet().size());
-
-                System.out.println("\nSHARED VARIABLE PERCENTAGE: " + svar_percent);
-                System.out.println("SHARED ARRAY PERCENTAGE: " + sarray_percent);
-            }
-            // save the sharedvariable to database??
-            RecordRT.saveSharedMetaData(db, sharedVariables, sharedArrayLocations);
-        }
     }
 
     public int getVariableId(String sig) {

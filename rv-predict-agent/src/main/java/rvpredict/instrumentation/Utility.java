0da80a30f1699400368f1924a3b59dc126367fff
Yilong Li <yli147@illinois.edu>
2014-11-20 20:59:23 -0600

    special treatment for loading class literals in class with version smaller than 49.0

--- rv-predict-agent/src/main/java/rvpredict/instrumentation/Utility.java
+++ rv-predict-agent/src/main/java/rvpredict/instrumentation/Utility.java
@@ -18,6 +18,7 @@ public class Utility {
     public static final String DESC_CHAR   =   Type.CHAR_TYPE.getDescriptor();
     public static final String DESC_FLOAT  =   Type.FLOAT_TYPE.getDescriptor();
     public static final String DESC_DOUBLE =   Type.DOUBLE_TYPE.getDescriptor();
+    public static final String DESC_CLASS  =   "Ljava/lang/Class;";
 
     public static final String DESC_ARRAY_PREFIX    =   "[";
     public static final String DESC_OBJECT_PREFIX   =   "L";

4c31ad6dd8f138b41f6dc955644b0749176c1515
Traian Florin Serbanuta <traian.serbanuta@gmail.com>
2015-01-22 13:00:17 +0000

    Removed additional import.

--- rv-predict/src/test/java/com/runtimeverification/rvpredict/engine/main/MainTest.java
+++ rv-predict/src/test/java/com/runtimeverification/rvpredict/engine/main/MainTest.java
@@ -11,7 +11,6 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-import rvpredict.trace.SyncEvent;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;

99b56fe0f47a1294c2f4f11cf492ed2c3586d715
Yilong Li <yli147@illinois.edu>
2015-03-13 15:05:34 -0500

    removed more actually unused code

--- rv-predict-logging/src/main/java/com/runtimeverification/rvpredict/util/Logger.java
+++ rv-predict-logging/src/main/java/com/runtimeverification/rvpredict/util/Logger.java
@@ -3,49 +3,14 @@ package com.runtimeverification.rvpredict.util;
 import com.google.common.base.Strings;
 import com.runtimeverification.rvpredict.config.Configuration;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-
 /**
- * Created by TraianSF on 05.08.2014.
+ * @author TraianSF
+ * @author YilongL
  */
 public class Logger {
 
-    public static final int WIDTH = 75;
-    public static final String DASH = "-";
-
-    private PrintWriter out;
-    Configuration config;
-
-    /**
-     * Initialize the file printer. All race detection statistics are stored
-     * into the file race-report.txt
-     *
-     * @param config
-     */
-    public Logger(Configuration config) {
-        this.config = config;
-        try {
-            String fname = "race-report.txt";
-
-            File file = new File(config.outdir);
-            file.mkdirs();
-            out = new PrintWriter(new FileWriter(config.outdir + "/" + fname, true));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void closePrinter() {
-        if (out != null)
-            out.close();
-    }
-
-    public PrintWriter getPrinter() {
-        return out;
-    }
+    private static final int WIDTH = 75;
+    private static final String DASH = "-";
 
     public static String center(String msg) {
         int fillWidth = WIDTH - msg.length();
@@ -61,25 +26,15 @@ public class Logger {
         switch (type) {
         case ERROR:
             System.err.println(msg);
-            out.println(msg);
             break;
         case REAL:
         case INFO:
             System.out.println(msg);
-            if (config.verbose) {
-                out.println(msg);
-            }
             break;
         case STATISTICS:
-            if (config.verbose) {
-                System.out.println(msg);
-            }
-            out.println(msg);
-            break;
         case VERBOSE:
-            if (config.verbose) {
+            if (Configuration.verbose) {
                 System.out.println(msg);
-                out.println(msg);
             }
             break;
         case POTENTIAL:

9802ba5b2f9561e73cbeda6fc52b5fde446ac2f1
Traian Florin Serbanuta <traian.serbanuta@gmail.com>
2015-11-17 05:12:50 -0600

    Using gdb to get address and location info

--- bin/kernel-process.py
+++ bin/kernel-process.py
@@ -15,6 +15,71 @@ import sys
 from enum import Enum
 from collections import deque, OrderedDict
 from sys import argv
+from subprocess import Popen, PIPE
+import os
+
+def startGDB(module):
+    gdb = Popen(['/usr/bin/gdb', '--quiet'], stderr=PIPE,stdout = PIPE, stdin = PIPE )
+    #gdb.stdin.write("set print symbol-filename on\n")
+    with open(".sections/.text","r") as f:
+        text = f.read()
+    gdb.stdin.write("add-symbol-file " + module + " " + text[:-1])
+    count = 1
+    for filename in os.listdir(".sections"):
+        info("segment", filename)
+        if filename.startswith("."):
+            if filename in {".text", ".strtab", ".symtab"}:
+                continue
+            count += 1
+            with open(".sections/"+filename,"r") as f:
+                section = f.read()
+                info("(gdb)"," -s " + filename + " " + section)
+                gdb.stdin.write(" -s " + filename + " " + section[:-1])
+    gdb.stdin.write("\n")
+    for i in range(0,count+3):
+        line = gdb.stdout.readline()
+        info("(gdb) ", line)
+    #gdb.stdin.write("info address 0\n")
+    #line = gdb.stdout.readline()
+    #info("(gdb) ", line)
+    return gdb
+
+def lookupAddress(gdb,addr):
+    addr = "0x" + addr[1:]
+    if addr in lookupAddress.__cache:
+        return lookupAddress.__cache[addr]
+    gdb.stdin.write("info symbol " + addr + "\n")
+    info("gdb<<", "info symbol " + addr + "\n")
+    symbol = gdb.stdout.readline()
+    if symbol.startswith("(gdb) No symbol matches"):
+        symbol = addr
+    else:
+        symbol = symbol.split()[1]
+    info("gdb>>", symbol)
+    lookupAddress.__cache[addr] = symbol
+    return symbol
+lookupAddress.__cache = {}
+
+def lookupLocation(gdb,loc):
+    if loc in lookupLocation.__cache:
+        return lookupLocation.__cache[loc]
+    symbol = lookupAddress(gdb, "."+loc)
+    info("gdb<<", "info symbol 0x" + loc + "\n")
+    gdb.stdin.write("info line *0x" + loc + "\n")
+    info("gdb<<", "info line *0x" + loc + "\n")
+    line = gdb.stdout.readline()
+    if line.startswith('(gdb) No line number '):
+        file = line = "???"
+    else:
+        parts = line.split()
+        line = parts[2]
+        file = parts[4]
+        gdb.stdout.readline() 
+    info("gdb>>", line)
+    lookupLocation.__cache[loc] = (symbol,file, line)
+    return (symbol,file, line)
+lookupLocation.__cache = {}
+
 
 def info(*objs):
     print("INFO: ", objs, file=sys.stderr)
@@ -62,7 +127,7 @@ class LocSig:
 
 class Field(LocSig):
     def __str__(self):
-        return "field {locSig}".format(locSig = self.locSig)
+        return "{locSig}".format(locSig = self.locSig)
 
 class Array(LocSig):
     def __str__(self):
@@ -70,22 +135,26 @@ class Array(LocSig):
 
 class Access:
     def __str__(self):
-        return "    Concurrent {type} in thread {thread} (locks held: {{{locks}}})\n" \
+        return "    Concurrent {type} in {thread} (locks held: {{{locks}}})\n" \
                " ---->  {locations}\n" \
-               "{thread_info}".\
+               .\
             format(type = self.type,
-                   thread = self.thread,
+                   thread = self.formatThread(),
                    locks = self.locks,
                    locations = "\n        ".join([str(x) for x in self.stack]),
-                   thread_info = self.thread_info)
+                   )
+    def formatThread(self):
+        if (self.thread == "T9223372036854775807"):
+            return "interrupt context"
+        return "thread " + self.thread
 
 class Location:
     def __init__(self,class_name,file_name,file_line):
         self.class_name = class_name
         self.file_name = file_name
         self.file_line = file_line
-    def __init__(self,locId):
-        self.locId = locId
+    #def __init__(self,locId):
+    #    self.locId = locId
     def __str__(self):
         return "at {cname}({fname}:{fline})".\
             format(cname = self.class_name, fname = self.file_name, fline = self.file_line)
@@ -111,8 +180,6 @@ thread_main_re = re.compile("\s*([^ ]*) is the main thread");
 race_end_re = re.compile("[}][}][}]")
 
 races = {}
-addresses = set()
-locations = set()
 
 def finalize_leg():
     global state
@@ -125,8 +192,9 @@ def finalize_leg():
         state = State.final
 
 
-script, filename = argv
+script, filename, module = argv
 txt = open(filename)
+gdb = startGDB(module)
 
 for line in txt:
     info(str(state) + "\t" + line)
@@ -135,14 +203,12 @@ for line in txt:
         match = field_race_start_re.match(line)
         if match:
             race = Race()
-            race.locSig = Field(match.group(1))
-            addresses.add(match.group(1))
+            race.locSig = Field(lookupAddress(gdb,match.group(1)))
         else:
             match = array_race_start_re.match(line)
             if match:
                 race = Race()
-                race.locSig = Array(match.group(1))
-                addresses.add(match.group(1))
+                race.locSig = Array(lookupAddress(gdb,match.group(1)))
         if match:
             race_key = match.group(1)
             race.left = None
@@ -163,20 +229,19 @@ for line in txt:
         match = first_stack_re.match(line)
         assert match, line
         access.stack = deque();
-        access.stack.append(Location(*match.groups()))
-        locations.add(*match.groups())
+        location = lookupLocation(gdb,*match.groups())
+        race_key+= ":"+location[2]
+        access.stack.append(Location(*location))
         state = State.leg_stack
         continue
     if state == State.leg_stack:
         match = next_stack_re.match(line)
         if match:
-            access.stack.append(Location(*match.groups()))
-            locations.add(*match.groups())
+            access.stack.append(Location(*lookupLocation(gdb,*match.groups())))
             continue
         match = lock_stack_re.match(line)
         if match:
-            access.stack.append(LockLocation(*match.groups()))
-            locations.add(*match.groups())
+            access.stack.append(LockLocation(*lookupLocation(gdb,*match.groups())))
             continue
         state = State.leg_thread
     if state == State.leg_thread:
@@ -196,8 +261,7 @@ for line in txt:
         match = next_stack_re.match(line)
         finalize_leg()
         if match:
-            creation_location = Location(*match.groups())
-            locations.add(*match.groups())
+            creation_location = Location(*lookupLocation(gdb,*match.groups()))
             access.thread_info = CreatedThreadInfo(parent_thread, creation_location, access.thread)
             continue
     if state == State.leg_final:
@@ -209,9 +273,5 @@ for line in txt:
         state = State.initial
 
 oraces = OrderedDict(sorted(races.items()))
-#for race in oraces.itervalues():
-#    print(race,"\n")
-for varId in addresses:
-    print(varId,"\n")
-for locId in locations:
-    print(locId,"\n")
+for race in oraces.itervalues():
+    print(race,"\n")

6e6f29d7ed209e44d8eb918cc4a764ceb79e5298
David Young <dyoung@pobox.com>
2016-11-16 16:30:38 -0600

    The LLVM pass inserts __rvpredict_* calls rather than __tsan_* calls.  Change the runtime to match.

--- lib/tsan/rtl/tsan_interface.h
+++ lib/tsan/rtl/tsan_interface.h
@@ -19,7 +19,7 @@
 #include <sanitizer_common/sanitizer_internal_defs.h>
 
 // This header should NOT include any other headers.
-// All functions in this header are extern "C" and start with __tsan_.
+// All functions in this header are extern "C" and start with __rvpredict_.
 
 #ifdef __cplusplus
 extern "C" {
@@ -27,53 +27,53 @@ extern "C" {
 
 // This function should be called at the very beginning of the process,
 // before any instrumented code is executed and before any call to malloc.
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_init();
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_init();
 
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_read1(void *addr);
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_read2(void *addr);
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_read4(void *addr);
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_read8(void *addr);
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_read16(void *addr);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_read1(void *addr);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_read2(void *addr);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_read4(void *addr);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_read8(void *addr);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_read16(void *addr);
 
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_write1(void *addr, void *val);
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_write2(void *addr, void *val);
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_write4(void *addr, void *val);
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_write8(void *addr, void *val);
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_write16(void *addr, void *val);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_write1(void *addr, void *val);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_write2(void *addr, void *val);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_write4(void *addr, void *val);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_write8(void *addr, void *val);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_write16(void *addr, void *val);
 
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_unaligned_read2(const void *addr);
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_unaligned_read4(const void *addr);
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_unaligned_read8(const void *addr);
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_unaligned_read16(const void *addr);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_unaligned_read2(const void *addr);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_unaligned_read4(const void *addr);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_unaligned_read8(const void *addr);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_unaligned_read16(const void *addr);
 
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_unaligned_write2(void *addr, void *val);
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_unaligned_write4(void *addr, void *val);
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_unaligned_write8(void *addr, void *val);
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_unaligned_write16(void *addr, void *val);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_unaligned_write2(void *addr, void *val);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_unaligned_write4(void *addr, void *val);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_unaligned_write8(void *addr, void *val);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_unaligned_write16(void *addr, void *val);
 
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_read1_pc(void *addr, void *pc);
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_read2_pc(void *addr, void *pc);
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_read4_pc(void *addr, void *pc);
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_read8_pc(void *addr, void *pc);
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_read16_pc(void *addr, void *pc);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_read1_pc(void *addr, void *pc);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_read2_pc(void *addr, void *pc);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_read4_pc(void *addr, void *pc);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_read8_pc(void *addr, void *pc);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_read16_pc(void *addr, void *pc);
 
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_write1_pc(void *addr, void *pc, void *val);
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_write2_pc(void *addr, void *pc, void *val);
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_write4_pc(void *addr, void *pc, void *val);
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_write8_pc(void *addr, void *pc, void *val);
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_write16_pc(void *addr, void *pc, void *val);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_write1_pc(void *addr, void *pc, void *val);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_write2_pc(void *addr, void *pc, void *val);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_write4_pc(void *addr, void *pc, void *val);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_write8_pc(void *addr, void *pc, void *val);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_write16_pc(void *addr, void *pc, void *val);
 
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_vptr_read(void **vptr_p);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_vptr_read(void **vptr_p);
 SANITIZER_INTERFACE_ATTRIBUTE
-void __tsan_vptr_update(void **vptr_p, void *new_val);
+void __rvpredict_vptr_update(void **vptr_p, void *new_val);
 
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_func_entry(void *call_pc);
-SANITIZER_INTERFACE_ATTRIBUTE void __tsan_func_exit();
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_func_entry(void *call_pc);
+SANITIZER_INTERFACE_ATTRIBUTE void __rvpredict_func_exit();
 
 SANITIZER_INTERFACE_ATTRIBUTE
-void __tsan_read_range(void *addr, unsigned long size);  // NOLINT
+void __rvpredict_read_range(void *addr, unsigned long size);  // NOLINT
 SANITIZER_INTERFACE_ATTRIBUTE
-void __tsan_write_range(void *addr, unsigned long size);  // NOLINT
+void __rvpredict_write_range(void *addr, unsigned long size);  // NOLINT
 
 #ifdef __cplusplus
 }  // extern "C"
--- lib/tsan/rtl/tsan_interface_inl.h
+++ lib/tsan/rtl/tsan_interface_inl.h
@@ -18,94 +18,94 @@
 
 using namespace __tsan;  // NOLINT
 
-void __tsan_read1(void *addr) {
+void __rvpredict_read1(void *addr) {
   RVReadInteger((uptr)addr, 1, CALLERPC);
 }
 
-void __tsan_read2(void *addr) {
+void __rvpredict_read2(void *addr) {
   RVReadInteger((uptr)addr, 2, CALLERPC);
 }
 
-void __tsan_read4(void *addr) {
+void __rvpredict_read4(void *addr) {
   RVReadInteger((uptr)addr, 4, CALLERPC);
 }
 
-void __tsan_read8(void *addr) {
+void __rvpredict_read8(void *addr) {
   RVReadInteger((uptr)addr, 8, CALLERPC);
 }
 
-void __tsan_write1(void *addr, void *val) {
+void __rvpredict_write1(void *addr, void *val) {
   RVWriteInteger((uptr)addr, 1, CALLERPC, val);
 }
 
-void __tsan_write2(void *addr, void *val) {
+void __rvpredict_write2(void *addr, void *val) {
   RVWriteInteger((uptr)addr, 2, CALLERPC, val);
 }
 
-void __tsan_write4(void *addr, void *val) {
+void __rvpredict_write4(void *addr, void *val) {
   RVWriteInteger((uptr)addr, 4, CALLERPC, val);
 }
 
-void __tsan_write8(void *addr, void *val) {
+void __rvpredict_write8(void *addr, void *val) {
   RVWriteInteger((uptr)addr, 8, CALLERPC, val);
 }
 
-void __tsan_read1_pc(void *addr, void *pc) {
+void __rvpredict_read1_pc(void *addr, void *pc) {
   RVReadInteger((uptr)addr, 1, (uptr)pc);
 }
 
-void __tsan_read2_pc(void *addr, void *pc) {
+void __rvpredict_read2_pc(void *addr, void *pc) {
   RVReadInteger((uptr)addr, 2, (uptr)pc);
 }
 
-void __tsan_read4_pc(void *addr, void *pc) {
+void __rvpredict_read4_pc(void *addr, void *pc) {
   RVReadInteger((uptr)addr, 4, (uptr)pc);
 }
 
-void __tsan_read8_pc(void *addr, void *pc) {
+void __rvpredict_read8_pc(void *addr, void *pc) {
   RVReadInteger((uptr)addr, 8, (uptr)pc);
 }
 
-void __tsan_write1_pc(void *addr, void *pc, void *val) {
+void __rvpredict_write1_pc(void *addr, void *pc, void *val) {
   RVWriteInteger((uptr)addr, 1, (uptr)pc, val);
 }
 
-void __tsan_write2_pc(void *addr, void *pc, void *val) {
+void __rvpredict_write2_pc(void *addr, void *pc, void *val) {
   RVWriteInteger((uptr)addr, 2, (uptr)pc, val);
 }
 
-void __tsan_write4_pc(void *addr, void *pc, void *val) {
+void __rvpredict_write4_pc(void *addr, void *pc, void *val) {
   RVWriteInteger((uptr)addr, 4, (uptr)pc, val);
 }
 
-void __tsan_write8_pc(void *addr, void *pc, void *val) {
+void __rvpredict_write8_pc(void *addr, void *pc, void *val) {
   RVWriteInteger((uptr)addr, 8, (uptr)pc, val);
 }
 
-void __tsan_vptr_update(void **vptr_p, void *new_val) {
+void __rvpredict_vptr_update(void **vptr_p, void *new_val) {
   CHECK_EQ(sizeof(vptr_p), 8);
   RVSaveMemAccEvent(WRITE, (uptr)vptr_p, (u64)new_val, CALLERPC);
 }
 
-void __tsan_vptr_read(void **vptr_p) {
+void __rvpredict_vptr_read(void **vptr_p) {
   CHECK_EQ(sizeof(vptr_p), 8);
   RVSaveMemAccEvent(WRITE, (uptr)vptr_p, *(u64*)vptr_p, CALLERPC);
 }
 
-void __tsan_func_entry(void *pc) {
+void __rvpredict_func_entry(void *pc) {
   ThreadState* thr = cur_thread();
   FuncEntry(thr, (uptr)pc);
 }
 
-void __tsan_func_exit() {
+void __rvpredict_func_exit() {
   ThreadState *thr = cur_thread();
   FuncExit(thr);
 }
 
-void __tsan_read_range(void *addr, uptr size) {
+void __rvpredict_read_range(void *addr, uptr size) {
   RVSaveMemoryAccessRange(READ, (uptr)addr, size, CALLERPC, false);
 }
 
-void __tsan_write_range(void *addr, uptr size) {
+void __rvpredict_write_range(void *addr, uptr size) {
   RVSaveMemoryAccessRange(WRITE, (uptr)addr, size, CALLERPC, false);
 }

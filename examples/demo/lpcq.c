55e58a813c027f26c8bca46d010ba62481f4f4e5
David Young <dyoung@pobox.com>
2016-11-22 14:14:01 -0600

    Add build products to .gitignore.
    Make a bunch of temporary changes to my BSD makefiles that you would not be
    interested in reading about.
    
    Add sources for a POSIX Threads-using C program, lpcq, that passes some numbers
    from a producer thread to a consumer thread through a linked queue.  The
    program accepts an option, -l, that enables synchronization of queue accesses
    with a mutex.  RV-Predict detects races if lpcq is run without -l, and lpcq may
    even corrupt the queue and timeout or crash.  Run with -l, the program always
    finishes successfully, and RV-Predict detects no races.

---
+++ examples/demo/lpcq.c
@@ -0,0 +1,85 @@
+#include <sched.h>	/* for sched_yield() */
+#include <sys/param.h>	/* for MAX */
+#include <limits.h>	/* for INT_MAX */
+#include <stdlib.h>	/* for NULL */
+#include "lpcq.h"
+
+static inline void * volatile *
+lpcq_nextp(int nextofs, void *item)
+{
+	return (void * volatile *)((char *)item + nextofs);
+}
+
+void
+lpcq_init(lpcq_t *q, int nextofs)
+{
+	q->nextofs = nextofs;
+	q->head = NULL;
+	q->tailp = &q->head;
+}
+
+void *
+lpcq_get(lpcq_t *q)
+{
+	int n = 0;
+
+	if (q->tailp == &q->head)
+		return NULL;
+
+	void *item;
+
+	item = q->head;
+
+	void * volatile *nextp = lpcq_nextp(q->nextofs, item);
+	q->head = *nextp;
+	if (q->tailp == nextp) {
+		q->tailp = &q->head;
+	}
+	return item;
+}
+
+lpcq_iter_t
+lpcq_getall(lpcq_t *q)
+{
+	if (q->tailp == &q->head)
+		return (lpcq_iter_t){.item = NULL, .nextofs = 0, .lastnextp = NULL};
+
+	void *item;
+	int n = 0;
+
+	void * volatile *otailp = q->tailp;
+	q->tailp = &q->head;
+
+	return (lpcq_iter_t){.item = item,
+	        .nextofs = q->nextofs,
+		.lastnextp = otailp};
+}
+
+void *
+lpcq_next(lpcq_iter_t *i)
+{
+	void *head = i->item;
+
+	if (head == NULL)
+		return NULL;
+
+	void * volatile *nextp = lpcq_nextp(i->nextofs, head);
+	if (nextp == i->lastnextp) {
+		i->item = NULL;
+		return head;
+	}
+
+	i->item = *nextp;
+	return head;
+}
+
+void
+lpcq_put(lpcq_t *q, void *item)
+{
+	void * volatile *nextp = lpcq_nextp(q->nextofs, item);
+
+	*nextp = NULL;
+	void * volatile *otailp = q->tailp;
+	q->tailp = nextp;
+	*otailp = item;
+}
---
+++ examples/demo/lpcq_main.c
@@ -0,0 +1,124 @@
+#include <assert.h>
+#include <err.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include "lpcq.h"
+
+typedef struct _item {
+	void *next;
+	int idx;
+} item_t;
+
+#ifndef __arraycount
+#define __arraycount(__a)	(sizeof(__a) / sizeof((__a)[0]))
+#endif
+
+pthread_mutex_t *mutexp = NULL;
+item_t items[5];
+
+const int nitems = __arraycount(items);
+
+static inline void
+acquire_queue(void)
+{
+	int rc;
+
+	if (mutexp == NULL)
+		return;
+
+	rc = pthread_mutex_lock(mutexp);
+	assert(rc == 0);
+}
+
+static inline void
+release_queue(void)
+{
+	int rc;
+
+	if (mutexp == NULL)
+		return;
+
+	rc = pthread_mutex_unlock(mutexp);
+	assert(rc == 0);
+}
+
+void *
+consume(void *arg)
+{
+	int nread;
+	lpcq_t *q = arg;
+
+	for (nread = 0; nread < nitems; nread++) {
+		int loops;
+		item_t *item;
+
+		for (loops = 0; loops < 10000; loops++) {
+			acquire_queue();
+			item = lpcq_get(q);
+			release_queue();
+			if (item != NULL) 
+				break;
+			sched_yield();
+		}
+		printf("read item %d\n", item->idx);
+	}
+
+	return NULL;
+}
+
+void *
+produce(void *arg)
+{
+	lpcq_t *q = arg;
+	int i;
+
+	for (i = 0; i < nitems; i++) {
+		acquire_queue();
+		lpcq_put(q, &items[i]);
+		release_queue();
+	}
+
+	return NULL;
+}
+
+static void
+usage(const char *progname)
+{
+	fprintf(stderr, "Usage: %s [-l]\n", progname);
+	exit(EXIT_FAILURE);
+}
+
+int
+main(int argc, char **argv)
+{
+	int i, opt;
+	lpcq_t q;
+	pthread_mutex_t mutex;
+
+	while ((opt = getopt(argc, argv, "l")) != -1) {
+		switch (opt) {
+		case 'l':
+			fprintf(stderr, "locking enabled\n");
+			mutexp = &mutex;
+			break;
+		default:
+			usage(argv[0]);
+		}
+	}
+
+	if (pthread_mutex_init(&mutex, NULL) != 0)
+		err(EXIT_FAILURE, "%s: pthread_mutex_init", __func__);
+
+	lpcq_init(&q, offsetof(item_t, next));
+	pthread_t producer, consumer;
+	for (i = 0; i < nitems; i++)
+		items[i].idx = i;
+	pthread_create(&consumer, NULL, &consume, &q);
+	pthread_create(&producer, NULL, &produce, &q);
+	pthread_join(producer, NULL);
+	pthread_join(consumer, NULL);
+	(void)pthread_mutex_destroy(&mutex);
+}

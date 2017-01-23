2bc80f3f70deab54cff7d3096464fe976910cfff
David Young <dyoung@pobox.com>
2017-01-23 12:00:08 -0600

    Use the dynamic linker to interpose our pthread(3) stubs, __rvpredict_pthread_*.

---
+++ llvm/ngrt/interpose.h
@@ -0,0 +1,34 @@
+#ifndef _RVP_INTERPOSE_H_
+#define _RVP_INTERPOSE_H_
+
+#include <dlfcn.h> /* for dlsym(3) */
+#include <pthread.h>
+
+#define	REAL_DEFN(__rettype, __func, ...)				\
+	__rettype (*real_##__func)(__VA_ARGS__)
+
+#define	INTERPOSE_DECLS(__rettype, __func, ...)				\
+extern __rettype (*real_##__func)(__VA_ARGS__);				\
+__rettype __rvpredict_##__func(__VA_ARGS__)
+
+#define	INTERPOSE(__rettype, __func, ...)				\
+__rettype __func(__VA_ARGS__) __attribute__((weak,			\
+			             alias("__rvpredict_" #__func),	\
+				     visibility("default")))
+
+#define	ESTABLISH_PTR_TO_REAL(__func)	do {		\
+	real_##__func = dlsym(RTLD_NEXT, #__func);	\
+} while (/*CONSTCOND*/false)
+
+INTERPOSE_DECLS(int, pthread_join, pthread_t, void **);
+INTERPOSE_DECLS(int, pthread_create, pthread_t *, const pthread_attr_t *,
+    void *(*)(void *), void *);
+INTERPOSE_DECLS(void, pthread_exit, void *);
+
+INTERPOSE_DECLS(int, pthread_mutex_lock, pthread_mutex_t *);
+INTERPOSE_DECLS(int, pthread_mutex_trylock, pthread_mutex_t *);
+INTERPOSE_DECLS(int, pthread_mutex_unlock, pthread_mutex_t *);
+INTERPOSE_DECLS(int, pthread_mutex_init, pthread_mutex_t *restrict,
+   const pthread_mutexattr_t *restrict);
+
+#endif /* _RVP_INTERPOSE_H_ */

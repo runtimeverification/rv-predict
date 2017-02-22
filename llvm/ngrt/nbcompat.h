92db875a156d5a3e0910ba8211b3a5c3b05df04a
David Young <david.young@runtimeverification.com>
2017-02-22 18:04:18 +0000

    Make the compatibility #definitions conditional on !defined(__NetBSD__). Is __NetBSD__ the right #define, though?
    Remove a redundant #definition of __aligned.

--- llvm/ngrt/nbcompat.h
+++ llvm/ngrt/nbcompat.h
@@ -3,6 +3,10 @@
 #ifndef _RVP_NBCOMPAT_H_
 #define _RVP_NBCOMPAT_H_
 
+#include <sys/param.h>
+
+#ifndef __NetBSD__
+
 #ifndef __arraycount
 #define __arraycount(__a)	(sizeof(__a) / sizeof((__a)[0]))
 #endif /* __arraycount */
@@ -15,12 +19,10 @@
 #define __section(x)	__attribute__((__section__(x)))
 #endif /* __section */
 
-#ifndef	__aligned
-#define __aligned(x)	__attribute__((__aligned__(x)))
-#endif /* __aligned */
-
 #ifndef __packed
 #define	__packed	__attribute__((__packed__))
 #endif /* __packed */
+
+#endif /* __NetBSD_Version__ */
 
 #endif /* _RVP_NBCOMPAT_H_ */

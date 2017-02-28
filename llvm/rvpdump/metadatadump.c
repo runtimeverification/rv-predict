1bb3f29194d017b44480836c99a0e02618cd4a9c
David Young <dyoung@pobox.com>
2017-02-28 15:56:02 -0600

    If a command-line argument is given, read metadata from the file it names instead of from stdin.

--- llvm/rvpdump/metadatadump.c
+++ llvm/rvpdump/metadatadump.c
@@ -5,17 +5,43 @@
 #include <stdlib.h>	/* for EXIT_FAILURE */
 #include <unistd.h>	/* for STDIN_FILENO */
 
+#include "nbcompat.h"
+
+static void __dead
+usage(const char *progname)
+{
+	fprintf(stderr, "usage: %s [trace-file]\n", progname);
+	exit(EXIT_FAILURE);
+}
+
 int
 main(int argc, char **argv)
 {
 	uint64_t id;
+	FILE *inf;
+	const char *progname = argv[0], *fname;
 
-	while (fread(&id, sizeof(id), 1, stdin) != 0) {
+	switch (argc) {
+	case 1:
+		inf = stdin;
+		break;
+	case 2:
+		fname = argv[1];
+		if ((inf = fopen(fname, "r")) == NULL) {
+			err(EXIT_FAILURE, "%s: fopen(\"%s\", ...)", __func__,
+			    fname);
+		}
+		break;
+	default:
+		usage(progname);
+	}
+
+	while (fread(&id, sizeof(id), 1, inf) != 0) {
 		char buf[1024];
 		char *p = &buf[0];
 
 		for (;;) {
-			int c = fgetc(stdin);
+			int c = fgetc(inf);
 			if (c == EOF) {
 				// records end with '\0', so quit with error
 				*p++ = '\0';
@@ -30,7 +56,7 @@ main(int argc, char **argv)
 			}
 		}
 	}
-	if (ferror(stdin) != 0)
+	if (ferror(inf) != 0)
 		errx(EXIT_FAILURE, "%s: error reading input", __func__);
 
 	return 0;

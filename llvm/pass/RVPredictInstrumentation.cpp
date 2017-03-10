49882570eb5b128b43113f6b76b918dc9bd1b260
Virgil Serbanuta <virgil@poarta.org>
2017-03-10 13:36:18 +0200

    Remove unneeded changes.

--- llvm/pass/RVPredictInstrumentation.cpp
+++ llvm/pass/RVPredictInstrumentation.cpp
@@ -276,8 +276,8 @@ RVPredictInstrument::initializeCallbacks(Module &m)
 			atomic_rmw[op][i] =
 			    checkSanitizerInterfaceFunction(
 			        m.getOrInsertFunction(rmw_name, void_type,
-                                ptr_type, type, type, memory_order_type,
-       	                        nullptr));
+				    ptr_type, type, type, memory_order_type,
+				    nullptr));
 		}
 
 		SmallString<32> atomic_cas_name("__rvpredict_atomic_cas" +

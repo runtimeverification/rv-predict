fa039d89ea1422bad354b4ea3066870a71d2a3c8
David Young <dyoung@pobox.com>
2017-03-17 22:30:12 -0500

    Use rvpc.

--- examples/rvp.mk
+++ examples/rvp.mk
@@ -8,13 +8,11 @@ PASS_DIR?=${HOME}/rv/rv-predict-legacy/llvm/build/pass
 RV_PREDICT_CXX_LDADD+=-L${RUNTIME_DIR} -lclang_rt.tsan_cxx-x86_64
 LDADD+=-L${RUNTIME_DIR} -lclang_rt.tsan-x86_64
 LDADD+=-ldl -lrt
-.else
-PASS_DIR?=${HOME}/rv/rv-predict/llvm/build/pass
-RUNTIME_DIR?=${HOME}/rv/rv-predict/llvm/ngrt
-LDADD+=-L${RUNTIME_DIR} -lrvprt
-LDADD+=-ldl
-.endif
 COPTS+=-Xclang -load -Xclang ${PASS_DIR}/rvpinstrument.so
+.else
+CC=rvpc
+CXX=rvpc
+.endif
 .endif
 
 LDADD+=-pthread

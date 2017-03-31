b3625102cbc012bfd69cb0f9f10720377421c0d4
Virgil Serbanuta <virgil@poarta.org>
2017-03-31 16:07:55 +0300

    Add a test for SignalMaskMemoizationReader

---
+++ rv-predict-source/src/test/java/com/runtimeverification/rvpredict/log/compact/readers/SignalMaskMemoizationReaderTest.java
@@ -0,0 +1,68 @@
+package com.runtimeverification.rvpredict.log.compact.readers;
+
+import com.runtimeverification.rvpredict.log.compact.CompactEvent;
+import com.runtimeverification.rvpredict.log.compact.CompactEventReader;
+import com.runtimeverification.rvpredict.log.compact.Context;
+import com.runtimeverification.rvpredict.log.compact.InvalidTraceDataException;
+import com.runtimeverification.rvpredict.log.compact.TraceHeader;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.nio.ByteBuffer;
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SignalMaskMemoizationReaderTest {
+    private static final long SIGNAL_MASK = 12345678901234L;
+    private static final int ORIGIN = 15;
+    private static final int SIGNAL_MASK_NUMBER = 6789;
+
+    @Mock
+    private TraceHeader mockTraceHeader;
+    @Mock private CompactEvent mockCompactEvent;
+    @Mock private Context mockContext;
+    @Mock private TraceHeader mockHeader;
+    @Mock private CompactEventReader mockCompactEventReader;
+
+    @Test
+    public void computesDataSize_UsesDefaultDataSize4() throws InvalidTraceDataException {
+        when(mockTraceHeader.getDefaultDataWidthInBytes()).thenReturn(4);
+        when(mockTraceHeader.getPointerWidthInBytes()).thenReturn(4);
+
+        SignalMaskMemoizationReader reader = new SignalMaskMemoizationReader();
+        Assert.assertEquals(16, reader.size(mockTraceHeader));
+    }
+
+    @Test
+    public void computesDataSize_UsesDefaultDataSize8() throws InvalidTraceDataException {
+        when(mockTraceHeader.getDefaultDataWidthInBytes()).thenReturn(8);
+        when(mockTraceHeader.getPointerWidthInBytes()).thenReturn(4);
+
+        SignalMaskMemoizationReader reader = new SignalMaskMemoizationReader();
+        Assert.assertEquals(24, reader.size(mockTraceHeader));
+    }
+
+    @Test
+    public void readsData() throws InvalidTraceDataException {
+        when(mockHeader.getDefaultDataWidthInBytes()).thenReturn(4);
+        when(mockHeader.getPointerWidthInBytes()).thenReturn(8);
+        when(mockCompactEventReader.signalMaskMemoization(mockContext, SIGNAL_MASK, ORIGIN, SIGNAL_MASK_NUMBER))
+                .thenReturn(Collections.singletonList(mockCompactEvent));
+
+        ByteBuffer buffer = ByteBuffer.allocate(24)
+                .putLong(SIGNAL_MASK).putInt(ORIGIN).putInt(SIGNAL_MASK_NUMBER).putLong(Long.MAX_VALUE);
+        buffer.rewind();
+
+        SignalMaskMemoizationReader reader = new SignalMaskMemoizationReader();
+        List<CompactEvent> events = reader.readEvent(mockContext, mockCompactEventReader, mockHeader, buffer);
+
+        Assert.assertEquals(1, events.size());
+        Assert.assertEquals(mockCompactEvent, events.get(0));
+    }
+}

5a1bae33bb3c214df647005a56b7de6a629d68ed
Virgil Serbanuta <virgil@poarta.org>
2017-03-31 09:40:38 +0300

    Fixed small warnings

--- rv-predict-source/src/main/java/com/runtimeverification/rvpredict/log/compact/datatypes/Address.java
+++ rv-predict-source/src/main/java/com/runtimeverification/rvpredict/log/compact/datatypes/Address.java
@@ -1,13 +1,10 @@
 package com.runtimeverification.rvpredict.log.compact.datatypes;
 
 import com.runtimeverification.rvpredict.log.compact.InvalidTraceDataException;
-import com.runtimeverification.rvpredict.log.compact.ReadableData;
 import com.runtimeverification.rvpredict.log.compact.TraceHeader;
-
-import java.nio.ByteBuffer;
 
 public class Address extends VariableInt {
     public Address(TraceHeader header) throws InvalidTraceDataException {
         super(header, header.getPointerWidthInBytes());
     }
-};
+}

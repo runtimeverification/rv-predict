31d76bf92078f1ef8a956ed7c965f21db6bf120b
David Young <dyoung@pobox.com>
2017-04-03 13:03:46 -0500

    Make this really simple, and use __builtin_dwarf_cfa() instead of __builtin_frame_address().

--- ouat/stackstruct/foo.c
+++ ouat/stackstruct/foo.c
@@ -12,24 +12,13 @@ foo(void)
 		int b;
 		int a;
 	} s = {0, 0};
-#ifdef use_q
-	int q;
-#endif
 
-/*
-	void *retaddr = __builtin_return_address(0);
-*/
-	void *frmaddr = __builtin_frame_address(0);
+	void *cfa = __builtin_dwarf_cfa(); /* == llvm.eh.dwarf.cfa */
 
-	printf("s.a, s.b = %d, %d\n", s.a, s.b);
-	printf("&s - __builtin_frame_address() = %td\n",
-	    (char *)&s - (char *)frmaddr);
+	printf("s.a, s.b = %d, %d, &s - cfa = %td\n", s.a, s.b,
+	    (char *)&s - (char *)cfa);
 	printf("&s = %p\n", &s);
-	printf("frame address = %p\n", frmaddr);
-#ifdef use_q
-	printf("&q - __builtin_frame_address() = %td\n",
-	    (char *)&q - (char *)frmaddr);
-#endif
+	printf("DWARF Canonical Frame Address (CFA) = %p\n", cfa);
 }
 
 

7f64979a17f857833f6d512e836067ef745f801a
David Young <dyoung@pobox.com>
2017-04-24 12:53:02 -0500

    Add a multidimensional array that I can test rvsyms against.

--- ouat/stackstruct/foo.c
+++ ouat/stackstruct/foo.c
@@ -10,14 +10,17 @@ foo(void)
 {
 	xyz_t xyz = {.x = {3, 4, 5}, .y = {6, 7, 8}, .z = {9, 10, 11}};
 	xyz_t array[3][4];
+	int iarray[7][5][2] = {{{1}}};
 
 	void *cfa = __builtin_dwarf_cfa(); /* == llvm.eh.dwarf.cfa */
 
-	printf("xyz.x.p, array[1][2].y.q = %d, %d, &s - cfa = %td\n", xyz.x.p, array[1][2].y.q,
+	printf("xyz.x.p, array[1][2].y.q, iarray[0][0][0] = %d, %d, %d, "
+	    "&s - cfa = %td\n", xyz.x.p, array[1][2].y.q, iarray[0][0][0],
 	    (char *)&xyz - (char *)cfa);
 	printf("&xyz = %p\n", &xyz);
 	printf("&cfa = %p\n", &cfa);
 	printf("&array[0] = %p\n", &array[0]);
+	printf("&iarray[0] = %p\n", &iarray[0]);
 	printf("DWARF Canonical Frame Address (CFA) = %p\n", cfa);
 }
 

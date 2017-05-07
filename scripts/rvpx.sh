7b93130bcaf6d4874b4de329ac0505ba092aceb3
David Young <dyoung@pobox.com>
2017-05-07 11:00:42 -0500

    Analyze the trace even if the program was cancelled with an error. TBD tell the user that they can signal again to cancel the analysis.

--- scripts/rvpx.sh
+++ scripts/rvpx.sh
@@ -5,8 +5,27 @@ set -e
 tmpdir=$(mktemp -d)
 exitcode=1
 
+progname=$1
+if [ ${progname##/} != ${progname} ]; then
+	progpath=${progname}
+else
+	progpath=$(pwd)/${progname}
+fi
+
+predict()
+{
+	cd $tmpdir
+	${RV_ROOT:-/usr/local}/bin/rvpdump -t legacy rvpredict.trace
+	rv-predict --offline --window 4000 --compact-trace --llvm-predict . 2>&1 | \
+	rvpsymbolize $progpath 1>&2
+}
+
 exit_hook()
 {
+	trap - EXIT ALRM HUP INT PIPE QUIT TERM
+
+	predict
+
 	for core in $(ls $tmpdir/*core 2> /dev/null); do
 		echo "$(basename $0): there are cores in $tmpdir/." 1>&2
 		exit $exitcode
@@ -24,22 +43,12 @@ set +i
 
 export RVP_TRACE_FILE=${tmpdir}/rvpredict.trace
 
-progname=$1
-if [ ${progname##/} != ${progname} ]; then
-	progpath=${progname}
-else
-	progpath=$(pwd)/${progname}
-fi
-
 set +e
 "$@"
 exitcode=$?
 set -e
 
-cd $tmpdir
-${RV_ROOT:-/usr/local}/bin/rvpdump -t legacy rvpredict.trace
-rv-predict --offline --window 4000 --llvm-predict . 2>&1 | \
-rvpsymbolize $progpath 1>&2
+predict
 
 trap - EXIT ALRM HUP INT PIPE QUIT TERM
 

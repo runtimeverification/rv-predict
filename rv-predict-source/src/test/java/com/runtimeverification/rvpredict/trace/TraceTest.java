349fc6253815ce116e06edf67b4d7cd0c35066a0
Virgil Serbanuta <virgil@poarta.org>
2017-06-07 20:33:38 +0300

    Fix merge issues.

--- rv-predict-source/src/test/java/com/runtimeverification/rvpredict/trace/TraceTest.java
+++ rv-predict-source/src/test/java/com/runtimeverification/rvpredict/trace/TraceTest.java
@@ -12,9 +12,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.mockito.stubbing.Answer;
 
 import java.util.ArrayList;
 import java.util.Arrays;

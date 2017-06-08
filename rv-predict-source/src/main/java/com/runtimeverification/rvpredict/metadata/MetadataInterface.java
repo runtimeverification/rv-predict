8c39e9edb7217d908fcabee06091674487e5113a
David Young <dyoung@pobox.com>
2017-06-08 17:43:49 -0500

    Put race reports on local variables into a format that rvpsymbolize can symbolize.  Refactor a bit: move getRaceLocationSig() from Race to MetadataInterface.  In the CompactMetadata class, make getRaceLocationSig put the instruction pointer and CFA of all of the relevant frames inside of the square brackets.
    Add a MetadataInterface method, getLocationPrefix(), that returns either
    the empty string or the text to insert before a location.  If we're
    producing a race report for the compact event trace, then the prefix is
    "".  Otherwise, it is "at ".

--- rv-predict-source/src/main/java/com/runtimeverification/rvpredict/metadata/MetadataInterface.java
+++ rv-predict-source/src/main/java/com/runtimeverification/rvpredict/metadata/MetadataInterface.java
@@ -1,7 +1,13 @@
 package com.runtimeverification.rvpredict.metadata;
 
+import com.runtimeverification.rvpredict.config.Configuration;
+import com.runtimeverification.rvpredict.log.ReadonlyEventInterface;
+import com.runtimeverification.rvpredict.trace.Trace;
+
 public interface MetadataInterface {
     String getLocationSig(long locationId);
+    String getRaceLocationSig(ReadonlyEventInterface e1, ReadonlyEventInterface e2, Trace trace, Configuration config);
+    String getLocationPrefix();
     void addOriginalThreadCreationInfo(long childOTID, long parentOTID, long locId);
     long getOriginalThreadCreationLocId(long otid);
     long getParentOTID(long otid);

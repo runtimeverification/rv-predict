52053afebf6ac5f8c0f07c06b18abade496ece1e
David Young <david.young@runtimeverification.com>
2017-06-27 13:21:08 -0500

    Make the release script agree with Jenkins' expectations: add an argument for a version number, and produce a file called rv-predict-c-${version}.tar.gz.

--- release.sh
+++ release.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
+set -e
+
+usage()
+{
+	echo "usage: $(basename $0) version" 1>&2
+	exit 1
+}
+
+[ $# -eq 1 ] || usage
+
 rm -f rv-predict.tar.gz
 mkcmake RELEASE=yes PREFIX= bin_targz
+mv rv-predict.tar.gz rv-predict-c-${version}.tar.gz
+
+exit 0

940a85c494d3bc35b8aca0be6634fa443df1dff6
David Young <dyoung@pobox.com>
2017-06-28 11:56:16 -0500

    These changes are just along for the ride: fix the top-level `tags` target, which was broken in elftoolchain/; delete some whitespace at the end of a line; make my test program, sigcount, a little more useful.

--- llvm/ngrt/sigcount.c
+++ llvm/ngrt/sigcount.c
@@ -1,25 +1,32 @@
 #include <err.h>
 #include <signal.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int
 main(int argc, char **argv)
 {
 	sigset_t ss;
 	int signum;
+	bool leading_errors = true; 
 
 	sigfillset(&ss);
 
 	for (signum = 0; ; signum++) {
 		switch (sigismember(&ss, signum)) {
 		case 1:
+			leading_errors = false;
 			printf("%d is a member\n", signum);
 			break;
 		case 0:
+			leading_errors = false;
 			printf("%d is NOT a member\n", signum);
 			break;
 		case -1:
-			warn("%d", signum);
+			if (leading_errors)
+				continue;
+			err(EXIT_FAILURE, "signal #%d", signum);
 			break;
 		}
 	}

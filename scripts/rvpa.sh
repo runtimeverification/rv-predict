b6b91a1fa815627bb5079a8233f57f515519efd4
David Young <dyoung@pobox.com>
2017-07-05 10:40:36 -0500

    Add to rvpa, rvpx, and rvpsymbolize a --filter option that tells a keyword list of filters to disable: no-symbol, no-system, no-trim, no-signal. Implement no-symbol, no-trim, and no-signal.
    no-symbol disables symbolization altogether.
    
    no-signal disables conversion from RV-Predict's signal-number
    representation, S1..Sn, to UNIX symbols like SIGHUP, SIGSEGV.
    
    no-trim leaves in symbols below main() on the stack, and it leaves in
    symbols that belong to the RV-Predict runtime.
    
    no-system, which is not implemented yet, I intend to stop the
    symbolization from filtering symbols that are defined in system header
    files and libraries.  There is no such filtering, now.
    
    While I am here, move shell functions nearer to the top of my scripts,
    and move the equivalent of the "main" routine to the bottom.
    
    Pass --window N options to the backend.
    
    Add some notes about the --filter option to the unfinished manual in
    doc/manual.

--- scripts/rvpa.sh
+++ scripts/rvpa.sh
@@ -4,18 +4,9 @@ set -e
 
 usage()
 {
-	echo "usage: $(basename $0) program" 1>&2
+	echo "usage: $(basename $0) --filter [no-symbol,no-system,no-trim,no-signal] program" 1>&2
 	exit 1
 }
-
-[ $# -eq 1 ] || usage
-
-progname=$1
-if [ ${progname##/} != ${progname} ]; then
-	progpath=${progname}
-else
-	progpath=$(pwd)/${progname}
-fi
 
 rvpredict()
 {
@@ -55,5 +46,91 @@ EOF
 	${_java} -ea -jar ${libdir}/rv-predict.jar "$@"
 }
 
-rvpredict --offline --window 2000 --detect-interrupted-thread-race --compact-trace --llvm-predict . 2>&1 | \
-rvpsymbolize $progpath 1>&2
+trim_stack()
+{
+	# TBD suppress __rvpredict_ and rvp_ symbols first by
+	# converting to, say, ##suppressed##, then removing ##suppressed##
+	# and stanzas consisting only of ##suppressed## in a second stage
+	awk 'BEGIN { saw_stack_bottom = 0 }
+	/^ {6,6}[> ] in rvp_[a-zA-Z_][0-9a-zA-Z_]* at / {
+		saw_stack_bottom = 1
+		next
+	}
+	/^ {6,6}[> ] in __rvpredict_[a-zA-Z_][0-9a-zA-Z_]* at / {
+		saw_stack_bottom = 1
+		next
+	}
+	/^ {6,6}[> ] in main at / {
+		print
+		saw_stack_bottom = 1
+		next
+	}
+	/^ {0,7}[^ ]/ {
+		saw_stack_bottom = 0
+	}
+	/^$/ {
+		saw_stack_bottom = 0
+	}
+	{
+		if (!saw_stack_bottom)
+			print
+	}'
+}
+
+symbolize()
+{
+	if [ ${filter_symbol:-yes} = yes -a ${filter_trim:-yes} = yes ]
+	then
+		rvpsymbolize "$@" | trim_stack
+	elif [ ${filter_symbol:-yes} = yes ]
+	then
+		rvpsymbolize "$@"
+	else
+		cat
+	fi
+}
+
+symbolize_passthrough=
+
+while [ $# -gt 1 ]; do
+	case $1 in
+	--filter)
+		shift
+		for filt in $(echo $1 | sed 's/,/ /g'); do
+			case $filt in
+			no-trim|no-symbol)
+				eval filter_${filt##no-}=no
+				;;
+			*)
+				symbolize_passthrough="${symbolize_passthrough:-} --filter ${filt}"
+				;;
+			esac
+		done
+		shift
+		;;
+	--window)
+		shift
+		window="--window $1"
+		shift
+		;;
+	--)
+		shift
+		break
+		;;
+	*)	break
+		;;
+	esac
+done
+
+[ $# -eq 1 ] || usage
+
+progname=$1
+if [ ${progname##/} != ${progname} ]; then
+	progpath=${progname}
+else
+	progpath=$(pwd)/${progname}
+fi
+
+rvpredict --offline ${window:---window 2000} --detect-interrupted-thread-race \
+    --compact-trace --llvm-predict . 3>&2 2>&1 1>&3 3>&- | \
+    symbolize ${symbolize_passthrough} $progpath 1>&2

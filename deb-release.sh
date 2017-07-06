cf9ddf77858fbe9e4a03b366db3b132858fb33ec
David Young <dyoung@pobox.com>
2017-07-06 16:38:43 -0500

    Use fakeroot(1) to avoid constructing the Debian binary package as root.

--- deb-release.sh
+++ deb-release.sh
@@ -3,12 +3,15 @@
 set -e
 set -u
 
-destdir=$(mktemp -d -t $(basename $0).XXXXXX)
+destdir=$(mktemp -d -t $(basename $0).destdir.XXXXXX)
+tmpdir=$(mktemp -d -t $(basename $0).XXXXX)
+rootfifo=${tmpdir}/rootfifo
+rootsave=${tmpdir}/rootsave
 
 cleanup()
 {
 	trap - EXIT ALRM HUP INT PIPE QUIT TERM
-	rm -rf ${destdir}
+	rm -rf ${destdir} ${tmpdir}
 }
 
 trap cleanup EXIT ALRM HUP INT PIPE QUIT TERM
@@ -17,8 +20,13 @@ if ! type mkcmake > /dev/null 2>&1; then
 	export PATH=${HOME}/pkg/bin:${PATH}
 fi
 
-mkcmake RELEASE=yes DESTDIR=${destdir} PREFIX=/usr install
-cp -rp DEBIAN ${destdir}/.
-chown -R root:root ${destdir}/DEBIAN
-dpkg-deb -b ${destdir} rv-predict-c.deb
+mkfifo ${rootfifo}
+tail -f ${rootfifo} > ${rootsave} &
+
+# avoid creating files & directories with permissions 0775 
+umask 022
+mkcmake FAKEROOT_FIFO=${rootfifo} RELEASE=yes DESTDIR=${destdir} PREFIX=/usr install
+fakeroot -s ${rootfifo} cp -rp DEBIAN ${destdir}/.
+fakeroot -s ${rootfifo} chown -R root:root ${destdir}/DEBIAN
+fakeroot -i ${rootsave} dpkg-deb -b ${destdir} rv-predict-c.deb
 exit 0

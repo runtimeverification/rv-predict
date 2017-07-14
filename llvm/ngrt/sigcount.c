bedc5829346fc8eae14b5a4f1e73049e09a816aa
David Young <dyoung@pobox.com>
2017-07-14 09:07:05 -0500

    Print the signals that are in the default mask.

--- llvm/ngrt/sigcount.c
+++ llvm/ngrt/sigcount.c
@@ -8,26 +8,45 @@ int
 main(int argc, char **argv)
 {
 	sigset_t ss;
-	int signum;
-	bool leading_errors = true; 
+	int first, last, signum;
+	bool leading_errors = true;
+	sigset_t oldmask;
 
+	pthread_sigmask(SIG_BLOCK, NULL, &oldmask);
 	sigfillset(&ss);
 
 	for (signum = 0; ; signum++) {
 		switch (sigismember(&ss, signum)) {
 		case 1:
-			leading_errors = false;
+			if (leading_errors) {
+				first = signum;
+				leading_errors = false;
+			}
 			printf("%d is a member\n", signum);
 			break;
 		case 0:
-			leading_errors = false;
+			if (leading_errors) {
+				first = signum;
+				leading_errors = false;
+			}
 			printf("%d is NOT a member\n", signum);
 			break;
 		case -1:
 			if (leading_errors)
 				continue;
-			err(EXIT_FAILURE, "signal #%d", signum);
-			break;
+			goto out;
+		}
+	}
+out:
+	last = signum;
+	printf("last (exclusive) signal, #%d\n", last);
+	for (signum = first; signum < last; signum++) {
+		if (sigismember(&ss, signum) != 1)
+			continue;
+		if (sigismember(&oldmask, signum) == 1) {
+			printf("%d is in the initial mask\n", signum);
+		} else {
+			printf("%d is NOT in the initial mask\n", signum);
 		}
 	}
 	return 0;

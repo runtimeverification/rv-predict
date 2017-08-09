95b01436e5253997192043c4248bcec7a4833777
David Young <dyoung@pobox.com>
2017-08-09 10:41:56 -0500

    Put variables that are marked __rv_register into their own data section. This change will not actually effect unless the proper linker script is used.

--- llvm/ngrt/intr_exports.h
+++ llvm/ngrt/intr_exports.h
@@ -1,6 +1,10 @@
 #ifndef _RV_INTR_EXPORTS_H_
 #define _RV_INTR_EXPORTS_H_
 
+#ifndef __section
+#define __section(x)    __attribute__((__section__(x)))
+#endif /* __section */
+
 void __rvpredict_intr_enable(void);
 void __rvpredict_intr_disable(void);
 void __rvpredict_isr_fire(void (*)(void));
@@ -14,6 +18,6 @@ void __rvpredict_isr_fire(void (*)(void));
 #define	__rv_interrupt_handler(__source, __priority)	\
 	__rv_interrupt_handler_arg_resolved(__source, __priority)
 
-#define	__rv_register	__annotate("rvp-register")
+#define	__rv_register	__annotate("rvp-register") __section(".data.registers")
 
 #endif /* _RV_INTR_EXPORTS_H_ */

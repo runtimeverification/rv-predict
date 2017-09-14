164bb8c3e5950d9bb11205eb87bde83f9f9e8c91
Virgil Serbanuta <virgil@poarta.org>
2017-09-15 01:33:14 +0300

    Ask for the sudo password inside the installer.
    * Add an install tools project that holds the script runner class.
    
    * Small fixes.

--- c-install/scripts/install.sh
+++ c-install/scripts/install.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 set -e
-set -x
 
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-sudo sh -c "RV_PREDICT_LICENSE_ACCEPTED='yes' dpkg -i \"$SCRIPTPATH/../rv-predict-c-$1.deb\"" || sudo apt-get install -f -y
-sudo sh -c "RV_PREDICT_LICENSE_ACCEPTED='yes' dpkg -i \"$SCRIPTPATH/../rv-predict-c-$1.deb\""
+sh -c "RV_PREDICT_LICENSE_ACCEPTED='yes' dpkg -i \"$SCRIPTPATH/../rv-predict-c-$1.deb\"" || apt-get install -f -y
+sh -c "RV_PREDICT_LICENSE_ACCEPTED='yes' dpkg -i \"$SCRIPTPATH/../rv-predict-c-$1.deb\""

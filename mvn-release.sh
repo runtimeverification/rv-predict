cf254558a0e02f9ad0ffe7bda61b1c2044f4a85c
David Young <dyoung@pobox.com>
2017-10-03 16:13:24 -0500

    Change it back: use release:perform instead of release:deploy.

--- mvn-release.sh
+++ mvn-release.sh
@@ -2,6 +2,6 @@
 
 # release:clean
 
-mvn -X -B release:prepare release:perform release:deploy \
+mvn -X -B release:prepare release:perform \
     -Dusername=git -DskipTests -Dobfuscate -DpreparationGoals="clean install" \
     -Darguments="-DskipTests -Dobfuscate"

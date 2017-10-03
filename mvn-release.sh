e0700e7ee4a1faecf5018e2c94700ba313fa3d48
David Young <dyoung@pobox.com>
2017-10-03 16:09:29 -0500

    Make a change that I hope will deploy the release to the website.

--- mvn-release.sh
+++ mvn-release.sh
@@ -2,6 +2,6 @@
 
 # release:clean
 
-mvn -X -B release:prepare release:perform \
+mvn -X -B release:prepare release:perform release:deploy \
     -Dusername=git -DskipTests -Dobfuscate -DpreparationGoals="clean install" \
     -Darguments="-DskipTests -Dobfuscate"

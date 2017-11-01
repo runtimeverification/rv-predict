37f4d2a28f7807e7664715acb13291e61b4386bb
David Young <dyoung@pobox.com>
2017-11-01 14:30:52 -0500

    Add some comments and update line numbers in expect.out to match.

--- llvm/test/trace/memcpy/memcpy.c
+++ llvm/test/trace/memcpy/memcpy.c
@@ -30,10 +30,15 @@ static union {
 int
 main(void)
 {
+	/* expect forwards copy */
 	memcpy(&disjoint.dst, &disjoint.src, sizeof(disjoint.dst));
 	printf("dst = %s\n", disjoint.dst);
+
+	/* expect backwards copy */
 	memcpy(&lap1.dst, &lap1.src, sizeof(lap1.dst));
 	printf("lap1 dst = %s\n", lap1.dst);
+
+	/* expect forwards copy */
 	memcpy(&lap2.dst, &lap2.src, sizeof(lap2.dst));
 	printf("lap2 dst = %s\n", lap2.dst);
 	return EXIT_SUCCESS;

ed5a7566c20173a00b7bed3f7292c8d26526cfdf
David Young <dyoung@pobox.com>
2017-11-08 15:58:43 -0600

    Add a test for unaligned accesses and extract shared script `normalize` from the `memcpy` test.

---
+++ llvm/test/trace/unaligned/unaligned.c
@@ -0,0 +1,26 @@
+#include <inttypes.h>	/* for PRIu32 */
+#include <string.h>
+#include <stdint.h>	/* for uint8_t, uint32_t */
+#include <stdio.h>
+#include <stdlib.h>
+#include "nbcompat.h"
+
+struct _pair;
+typedef struct _pair pair_t;
+
+struct _pair {
+	uint8_t misalign;
+	uint32_t a;
+	uint32_t b;
+} __packed;
+
+int
+main(void)
+{
+	volatile pair_t p = {.a = 5, .b = 7};
+
+	printf("%" PRIu32 " * %" PRIu32 " = %" PRIu32 "\n",
+	    p.a, p.b, p.a * p.b);
+
+	return EXIT_SUCCESS;
+}

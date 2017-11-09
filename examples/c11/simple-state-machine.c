ccc96eb75510bb40ab10504679e83bc308dcc978
David Young <dyoung@pobox.com>
2017-11-09 17:32:32 -0600

    Add a C11 version of the simple state machine whose C++11 version is in ../cpp11/.

---
+++ examples/c11/simple-state-machine.c
@@ -0,0 +1,59 @@
+// Copyright (c) 2016 Runtime Verification Inc. (RV-Predict Team). All Rights Reserved.
+
+#include <pthread.h>
+#include <sched.h>
+#include <stdbool.h>
+#include "nbcompat.h"
+
+pthread_mutex_t l = PTHREAD_MUTEX_INITIALIZER;
+bool ready = false;
+typedef enum { STOP, INIT, START } state_t;
+state_t state = STOP;
+
+void *
+init(void *arg __unused)
+{
+	pthread_mutex_lock(&l);
+	ready = true;
+	pthread_mutex_unlock(&l);
+	state = INIT;
+	pthread_mutex_lock(&l);
+	ready = true;
+	pthread_mutex_unlock(&l);
+	return NULL;
+}
+
+void *
+start(void *arg __unused)
+{
+	sched_yield();
+	pthread_mutex_lock(&l);
+	if (ready && state == INIT) {
+		state = START;
+	}
+	pthread_mutex_unlock(&l);
+	return NULL;
+}
+
+void *
+stop(void *arg __unused)
+{
+	pthread_mutex_lock(&l);
+	ready = false;
+	state = STOP;
+	pthread_mutex_unlock(&l);
+	return NULL;
+}
+
+int
+main()
+{
+	pthread_t t1, t2, t3;
+	pthread_create(&t1, NULL, init, NULL);
+	pthread_create(&t2, NULL, start, NULL);
+	pthread_create(&t3, NULL, stop, NULL);
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
+	pthread_join(t3, NULL);
+	return 0;
+}

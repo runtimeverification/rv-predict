7a08be5fd36f311ce06ab7cdf3e57363f17ae32e
David Young <dyoung@pobox.com>
2017-11-30 17:29:16 -0600

    Add tests for 32- and 64-bit atomic adds & subtracts.

---
+++ ctests/trace/rmw/sub/sub.c
@@ -0,0 +1,27 @@
+#include <assert.h>	/* for PRIu32 */
+#include <inttypes.h>	/* for PRIu32 */
+#include <stdatomic.h>	/* for uint8_t, uint32_t */
+#include <stdint.h>	/* for uint8_t, uint32_t */
+#include <stdio.h>
+#include <stdlib.h>
+#include "nbcompat.h"
+
+static const uint32_t initial_u32 = 0x100;
+static const uint64_t initial_u64 = (uint64_t)0xf << 32;
+
+static const uint32_t end_u32 = 0x41;
+static const uint64_t end_u64 = 0x43;
+
+int
+main(void)
+{
+	volatile _Atomic uint32_t u32 = initial_u32;
+	volatile _Atomic uint64_t u64 = initial_u64;
+
+	assert(atomic_fetch_sub(&u32, initial_u32 - end_u32) == initial_u32);
+	assert(atomic_fetch_sub(&u64, initial_u64 - end_u64) == initial_u64);
+	assert(u32 == end_u32);
+	assert(u64 == end_u64);
+
+	return EXIT_SUCCESS;
+}

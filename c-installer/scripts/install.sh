d4aeb70ac0f79b33e13c4284ed90851d52dfd050
shd101wyy <shd101wyy@sina.com>
2017-12-06 21:59:50 -0600

    Restore c-installer

--- c-installer/scripts/install.sh
+++ c-installer/scripts/install.sh
@@ -5,5 +5,5 @@ set -e
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-sh -c "RV_PREDICT_LICENSE_ACCEPTED='yes' dpkg -i \"$SCRIPTPATH/../rv-predict-c.deb\"" || apt-get install -f -y
-sh -c "RV_PREDICT_LICENSE_ACCEPTED='yes' dpkg -i \"$SCRIPTPATH/../rv-predict-c.deb\""
+sh -c "RV_PREDICT_LICENSE_ACCEPTED='yes' dpkg -i \"$SCRIPTPATH/../rv-predict-c-$1.deb\"" || apt-get install -f -y
+sh -c "RV_PREDICT_LICENSE_ACCEPTED='yes' dpkg -i \"$SCRIPTPATH/../rv-predict-c-$1.deb\""

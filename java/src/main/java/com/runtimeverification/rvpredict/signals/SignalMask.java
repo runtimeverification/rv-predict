e2ce79478fefa738ecd894363e90d0f11778176d
David Young <dyoung@pobox.com>
2017-12-14 22:02:29 -0600

    Use Signals.signalInMask() instead of Signals.signalIsEnabled(). This changes the sense of the code back to the old, correct way, but clarifies the meaning---for me, anyway. :-)

--- java/src/main/java/com/runtimeverification/rvpredict/signals/SignalMask.java
+++ java/src/main/java/com/runtimeverification/rvpredict/signals/SignalMask.java
@@ -26,7 +26,7 @@ public class SignalMask {
     SignalMask unblock(long signalMask) {
         SignalMask unblocked = new SignalMask(this);
         for (int i = 0; i < Constants.SIGNAL_NUMBER_COUNT; i++) {
-            if (Signals.signalIsEnabled(i, signalMask)) {
+            if (Signals.signalInMask(i, signalMask)) {
                 unblocked.maskBits[i] = SignalMaskBit.ENABLED;
             }
         }
@@ -36,7 +36,7 @@ public class SignalMask {
     SignalMask block(long signalMask) {
         SignalMask blocked = new SignalMask(this);
         for (int i = 0; i < Constants.SIGNAL_NUMBER_COUNT; i++) {
-            if (Signals.signalIsEnabled(i, signalMask)) {
+            if (Signals.signalInMask(i, signalMask)) {
                 blocked.maskBits[i] = SignalMaskBit.DISABLED;
             }
         }
@@ -75,12 +75,10 @@ public class SignalMask {
         for (int i = 0; i < Constants.SIGNAL_NUMBER_COUNT; i++) {
             switch (maskBits[i]) {
                 case ENABLED:
-                    assert Signals.signalIsEnabled(i, mask);
+                    assert Signals.signalIsEnabled(i, mask) : "expected signal #" + i + " enabled";
                     break;
                 case DISABLED:
-                    if (Signals.signalIsEnabled(i, mask))
-                        System.err.println("expected signal #" + i + " disabled");
-                    assert !Signals.signalIsEnabled(i, mask);
+                    assert !Signals.signalIsEnabled(i, mask) : "expected signal #" + i + " disabled";
                     break;
                 case UNKNOWN:
                     break;

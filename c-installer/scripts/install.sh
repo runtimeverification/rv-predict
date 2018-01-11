b80218fc1e6eaad0ef4db76732a31f258a2a964f
David Young <dyoung@pobox.com>
2018-01-11 17:22:35 -0600

    Use the right .deb name template throughout.

--- c-installer/scripts/install.sh
+++ c-installer/scripts/install.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 set -e
+set -u
 
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-sh -c "RV_PREDICT_LICENSE_ACCEPTED='yes' dpkg -i \"$SCRIPTPATH/../rv-predict-c-$1.deb\"" || apt-get install -f -y
-sh -c "RV_PREDICT_LICENSE_ACCEPTED='yes' dpkg -i \"$SCRIPTPATH/../rv-predict-c-$1.deb\""
+version=$1
+
+sh -c "RV_PREDICT_LICENSE_ACCEPTED='yes' dpkg -i \"$SCRIPTPATH/../rv-predict-c_version{1}-1_amd64.deb\"" || apt-get install -f -y
+sh -c "RV_PREDICT_LICENSE_ACCEPTED='yes' dpkg -i \"$SCRIPTPATH/../rv-predict-c_${version}-1_amd64.deb\""

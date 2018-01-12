559872a413bf4210f1afce21fab29df59b4d86a4
David Young <dyoung@pobox.com>
2018-01-11 19:36:18 -0600

    Turn off Maven's debug output.

--- mvn-release.sh
+++ mvn-release.sh
@@ -2,8 +2,8 @@
 
 # -DpreparationGoals="clean install" 
 # release:perform release:clean
-# -X
-mvn -X -U -B release:prepare release:perform \
+# for debug output, add -X
+mvn -U -B release:prepare release:perform \
     -DpreparationGoals="clean install" \
     -Dusername=git -DskipTests -Dobfuscate \
     -Darguments="-DskipTests -Dobfuscate" "$@"

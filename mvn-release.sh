d61898c05f84e64234b3f3dfabe02fdddd61f4c7
David Young <dyoung@pobox.com>
2018-01-31 18:03:49 -0600

    Check for uncommitted changes and start the key agent with the `release@scp` credentials.  Add the release keys---the private key is encrypted.

--- mvn-release.sh
+++ mvn-release.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
 
+set -e
+set -u
+
+if [ $(git status -bs | grep -v '^??' | wc -l) != 1 ]; then
+	cat 1>&2 <<EOF
+$(basename $0): you appear to have uncommitted changes.  Commit them
+before you run this script.
+EOF
+	exit 1
+fi
+
+if [ ${SSH_AUTH_SOCK:-none} = none ]; then
+	if [ ${_RECURSE:-ok} != ok ]; then
+		echo "$(basename $0): cannot start SSH key agent, ssh-agent" \
+		    1>&2
+		exit 1
+	fi
+	export _RECURSE=no
+	exec ssh-agent ./mvn-release.sh
+fi
+
+ssh-add keys/release-key
+
 # -DpreparationGoals="clean install" 
 # release:perform release:clean
 # for debug output, add -X

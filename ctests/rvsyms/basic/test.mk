e2dafeea509b3433e76e6135a7754bb0572b4bbc
David Young <dyoung@pobox.com>
2018-02-06 13:43:07 -0600

    Stop poisoning the test with the paths to rvpsymbolize and rvsyms.

--- ctests/rvsyms/basic/test.mk
+++ ctests/rvsyms/basic/test.mk
@@ -1,8 +1,6 @@
 .PHONY : test_output
 test_output:
 	@set -e; \
-	which rvpsymbolize ; \
-	which rvsyms ; \
 	${.OBJDIR}/basic | rvpsymbolize ${.OBJDIR}/basic | uniq -u
 #	${MAKE} ${MAKEFLAGS} distclean > /dev/null
 

a99558a1296ae61756a5a3c05bd50811ca0cd230
David Young <dyoung@pobox.com>
2018-02-06 11:53:06 -0600

    Temporarily commit some diagnostic code that I hope will show me where rvsyms and rvpsymbolize are found when dpkg-buildpackage runs the `test` target.

--- ctests/rvsyms/basic/test.mk
+++ ctests/rvsyms/basic/test.mk
@@ -1,6 +1,8 @@
 .PHONY : test_output
 test_output:
 	@set -e; \
+	which rvpsymbolize ; \
+	which rvsyms ; \
 	${.OBJDIR}/basic | rvpsymbolize ${.OBJDIR}/basic | uniq -u
 #	${MAKE} ${MAKEFLAGS} distclean > /dev/null
 

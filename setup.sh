758bbf4ce98fa6175d62d47566c36a0b8848369a
David Young <dyoung@pobox.com>
2018-02-16 16:16:26 -0600

    Look for rvpmake in the top-level source directory's subdirectory, bin/. Install it there, too.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 
 set -e
 set -u
-set -x
 
 export OPAMROOT="$(mkcmake -V '$(.OBJDIR)')/.opam"
 
@@ -12,6 +11,18 @@ opam switch 4.03.0
 eval $(opam config env)
 OPAMYES=true opam install ocp-ocamlres ocamlbuild-atdgen csv uri atdgen atdj
 
-install -o $(id -u) -g $(id -g) -m 0755 ./rvpmake.in rvpmake
+install -o $(id -u) -g $(id -g) -m 0555 bin/rvpmake.in bin/rvpmake
+
+home_slash=${HOME}/
+bindir=$(pwd)/bin
+home_relative_bindir=${bindir##${HOME}/}
+
+if [ ${home_relative_bindir} != ${bindir} ]; then
+	pathdir=\$HOME/${home_relative_bindir}
+else
+	pathdir=${bindir}
+fi
+
+echo "Setup is complete.  Add ${pathdir} to your PATH."
 
 exit 0

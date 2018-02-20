88e4f538a6e2eb09ee7efe4c242e7f9521c6eb24
David Young <dyoung@pobox.com>
2018-02-20 14:35:13 -0600

    Squashed 'nbtools/' content from commit a5cbc7e
    git-subtree-dir: nbtools
    git-subtree-split: a5cbc7ee309895ce18e5556aa7cc842f4901dacc

---
+++ compatlib/mkc_sys_signame.h
@@ -0,0 +1,21 @@
+/*
+ Copyright (c) 2015-2017 by Aleksey Cheusov
+
+ See LICENSE file in the distribution.
+*/
+
+#ifndef _MKC_SYS_SIGNAME_H_
+#define _MKC_SYS_SIGNAME_H_
+
+#include <signal.h>
+
+#if !HAVE_VAR_SYS_NSIG_SIGNAL_H
+extern const int sys_nsig;
+#endif
+
+#if !HAVE_VAR_SYS_SIGNAME_SIGNAL_H
+extern const char * * sys_signame__ (void);
+#define sys_signame sys_signame__()
+#endif
+
+#endif // _MKC_SYS_SIGNAME_H_

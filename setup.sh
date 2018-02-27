72af99ea4c609132bdbf94419318cec0b92971b3
David Young <dyoung@pobox.com>
2018-02-27 10:11:35 -0600

    Install `nb-{install,mtree,pax}` and `rvpmake` in `.tools`.  Make `rvpmake` look for itself in `.tools`, too.

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 set -e
 set -u
 
-export OPAMROOT="$(mkcmake -V '$(.OBJDIR)')/.opam"
+objdir=$(mkcmake -V '$(.OBJDIR)')
+export OPAMROOT="${objdir}/.opam"
 
 yes n | opam init
 opam update 
@@ -11,11 +12,20 @@ opam switch 4.03.0
 eval $(opam config env)
 OPAMYES=true opam install ocp-ocamlres ocamlbuild-atdgen csv uri atdgen atdj
 
-install -o $(id -u) -g $(id -g) -m 0555 bin/rvpmake.in bin/rvpmake
-
 home_slash=${HOME}/
-bindir=$(pwd)/bin
+bindir=${objdir}/.tools/bin
+mandir=${objdir}/.tools/man
 home_relative_bindir=${bindir##${HOME}/}
+home_relative_mandir=${mandir##${HOME}/}
+
+mkcmake PREFIX=${objdir}/.nbstage LINKSPREFIX=${objdir}/.tools -C nbtools \
+    cleandir \
+    all-mtree all-pax all-xinstall \
+    install-mtree install-pax install-xinstall \
+    cleandir-mtree cleandir-pax cleandir-xinstall
+
+install -o $(id -u) -g $(id -g) -m 0555 bin/rvpmake.in \
+    ${objdir}/.tools/bin/rvpmake
 
 if [ ${home_relative_bindir} != ${bindir} ]; then
 	pathdir=\$HOME/${home_relative_bindir}
@@ -23,6 +33,13 @@ else
 	pathdir=${bindir}
 fi
 
+if [ ${home_relative_mandir} != ${mandir} ]; then
+	manpathdir=\$HOME/${home_relative_mandir}
+else
+	manpathdir=${mandir}
+fi
+
 echo "Setup is complete.  Add ${pathdir} to your PATH."
+echo "Add ${manpathdir} to your MANPATH."
 
 exit 0

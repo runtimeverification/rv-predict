5c947f67e2191cd23f47f7a5adbcd1012acb9333
David Young <dyoung@pobox.com>
2018-03-13 13:52:38 -0500

    Add a demonstration for the Renesas 78k0 interrupt model.

---
+++ examples/interrupts/78k0.c
@@ -0,0 +1,58 @@
+#include <intr_exports.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "nbcompat.h"
+
+#define	DI()	__rvpredict_intr_disable()
+#define	EI()	__rvpredict_intr_enable()
+
+#define update(__lvalue) do {	\
+	(__lvalue) += 1;	\
+} while (/*CONSTCOND*/false)
+
+static int racy, racefree;
+
+static void __rv_interrupt_handler(irq0, 0)
+lopri_handler(void)
+{
+	const char msg[] = "low priority interrupt\n";
+
+	write(STDERR_FILENO, msg, sizeof(msg) - 1);
+
+	update(racefree);
+	EI();
+	update(racy);
+}
+
+static void __rv_interrupt_handler(irq1, 1)
+hipri_handler(void)
+{
+	const char msg[] = "high priority interrupt\n";
+
+	write(STDERR_FILENO, msg, sizeof(msg) - 1);
+
+	update(racefree);
+	EI();
+	update(racy);
+	DI();
+	update(racefree);
+}
+
+int
+main(void)
+{
+	EI();
+	update(racy);
+	DI();
+	update(racefree);
+	EI();
+	update(racy);
+	DI();
+	update(racefree);
+	EI();
+	update(racy);
+	return EXIT_SUCCESS;
+}

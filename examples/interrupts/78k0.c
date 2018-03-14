d9296994f0ca9711510e039a93d31525ac1fa1da
David Young <dyoung@pobox.com>
2018-03-14 15:58:24 -0500

    Bracket interrupt-handler entry and exit with messages on stdout.

--- examples/interrupts/78k0.c
+++ examples/interrupts/78k0.c
@@ -18,27 +18,31 @@ static int racy, racefree;
 static void __rv_interrupt_handler(irq0, 0)
 lopri_handler(void)
 {
-	const char msg[] = "low priority interrupt\n";
+	const char enter_msg[] = "( low priority interrupt\n";
+	const char exit_msg[] = "low priority interrupt )\n";
 
-	write(STDERR_FILENO, msg, sizeof(msg) - 1);
+	write(STDERR_FILENO, enter_msg, sizeof(enter_msg) - 1);
 
 	update(racefree);
 	EI();
 	update(racy);
+	write(STDERR_FILENO, exit_msg, sizeof(exit_msg) - 1);
 }
 
 static void __rv_interrupt_handler(irq1, 1)
 hipri_handler(void)
 {
-	const char msg[] = "high priority interrupt\n";
+	const char enter_msg[] = "( high priority interrupt\n";
+	const char exit_msg[] = "high priority interrupt )\n";
 
-	write(STDERR_FILENO, msg, sizeof(msg) - 1);
+	write(STDERR_FILENO, enter_msg, sizeof(enter_msg) - 1);
 
 	update(racefree);
 	EI();
 	update(racy);
 	DI();
 	update(racefree);
+	write(STDERR_FILENO, exit_msg, sizeof(exit_msg) - 1);
 }
 
 int

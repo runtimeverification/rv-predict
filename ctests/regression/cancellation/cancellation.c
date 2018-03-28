befb51c3dd9bfd69bae0511ff3b4091cd8e936e1
David Young <dyoung@pobox.com>
2018-03-28 11:02:46 -0500

    Replace an irrelevant comment with a relevant one.

--- ctests/regression/cancellation/cancellation.c
+++ ctests/regression/cancellation/cancellation.c
@@ -1,15 +1,6 @@
-/* The RV-Predict/C runtime has diagnostic code that calls `abort()` if
- * it detects a discrepancy between the expected interrupt/signal mask
- * (stored in `rvp_thread_t` member `t_intrmask`) and the old mask that
- * `pthread_sigmask()` reads back.  Frequently the discrepancy arose
- * because you cannot actually block SIGKILL and SIGSTOP.  So after a
- * program sets a mask that contains SIGKILL and SIGSTOP, it will read
- * back a mask that contains neither.  The runtime needs to account for
- * this, and it will.
- *
- * This program is designed to `abort()` and, as a side-effect, produce
- * an unexpected trace file, if the runtime is not accounting properly
- * for the unblockability of SIGKILL and SIGSTOP.
+/* This program simply enters an infinite loop.  It's intended to
+ * run until it is cancelled with `SIGINT`, so it doesn't block
+ * or ignore any signals.
  */
 
 #include <err.h>

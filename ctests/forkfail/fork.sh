320a9f3fded741ed0eee7dcfd0df8ab98ff0f593
David Young <dyoung@pobox.com>
2018-03-29 15:01:32 -0500

    Oops.  Quiet this script to fix the test.

--- ctests/forkfail/fork.sh
+++ ctests/forkfail/fork.sh
@@ -4,7 +4,6 @@ proclim=1
 while true; do
 (
 	ulimit -p $proclim
-	echo trying $proclim
 	./forkfail 2>&1 | grep 'RV-Predict/C could not start the analysis process'
 ) 2> /dev/null && exit 0
 	proclim=$((proclim + 1))

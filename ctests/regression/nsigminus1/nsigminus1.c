5058dfbd89497dcf07d83a7e6f5abdb484438810
David Young <dyoung@pobox.com>
2018-07-10 16:08:31 -0500

    Add a regression test for the bug that Maya Rashish found in 1.9.

---
+++ ctests/regression/nsigminus1/nsigminus1.c
@@ -0,0 +1,27 @@
+#include <err.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+/* Make sure that we can fetch the state of every signal using
+ * `sigaction` without crashing.  We used to (in version 1.9?)
+ * crash on signal 32.
+ */
+int
+main(void)
+{
+	/* XXX: AFAICT, no standard defines NSIG.  We should iterate over
+	 * the signals in a different way.
+	 */
+        for (int sig = 1; sig < NSIG; sig++) {
+                struct sigaction context;
+                if (sigaction(sig, NULL, &context) == -1)
+			printf("sig %d -> %s\n", sig, strerror(errno));
+		else
+			printf("sig %d\n", sig);
+        }
+        return 0;
+}

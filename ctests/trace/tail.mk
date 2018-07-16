9c5937b1bf124d093c189ac3bfd3beded8089b72
David Young <dyoung@pobox.com>
2018-07-16 14:24:21 -0500

    Remove Carlton's debug statements so that the expected and actual output agree.

--- ctests/trace/tail.mk
+++ ctests/trace/tail.mk
@@ -8,20 +8,16 @@ WARNS=4
 STRIPFLAG=
 
 
-COPTS+=-O3 -g -O0
+COPTS+=-O0 -g
 LDADD+=-pthread
 
 .PHONY: test_output
 
 test.trace: $(PROG)
-	echo "We who are about to ? :test.trace" 1>&2
 	@$(.OBJDIR)/$(PROG) > /dev/null
-	echo "test.trace xit" 1>&2
 
 test_output: test.trace
-	echo "We who are about to ? :test_output" 1>&2
 	@rvpdump -t symbol-friendly $(RVP_TRACE_FILE) | rvpsymbolize $(.OBJDIR)/$(PROG) | $(CTEST_dir)/normalize-humanized-trace
-	echo "test_output xit" 1>&2
 
 CLEANFILES+=test.trace
 

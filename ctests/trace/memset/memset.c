1333d8cbdd2d9843179b1e6733524dd82213bf55
Carlton Mills <Carlton.Mills@runtimeverification.com>
2018-07-31 20:04:00 -0500

    Added exhaustive test of memmove and memset (there was a memset error)

--- ctests/trace/memset/memset.c
+++ ctests/trace/memset/memset.c
@@ -2,26 +2,47 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <err.h>
 #include "nbcompat.h"
 
 
+#define sziibgn 8
+int iibgn  []={0,1,2,3,4,6,7,8};
+//int lngths []={1,2,3,4,5,7,8,9};
 
-char gstr [9];
+int testit(int start, int n2set){
+	char *cc = calloc(start+n2set+2, sizeof(char));
+	int ii;
+	char c = 0x41 + start; /* a,b,c,... */
 
+	memset(&cc[start], c, n2set*sizeof(char));
+
+	/* aa should contain zero or more 0x00 bytes, 'n2set' bytes of 'c', and a byte of zero */
+	/* leading zeros */
+	for(ii=0;ii<start;ii++){
+		if(cc[ii] != 0)
+			return EXIT_FAILURE;
+	}
+	/* Did everything get set correctly */
+	for(ii=start;ii<start+n2set;ii++){
+		if(cc[ii] != c)
+			return EXIT_FAILURE;
+	}
+	/* Make sure that too many didn't get copied */
+	if(cc[start+n2set] != 0)
+		return EXIT_FAILURE;
+
+	return EXIT_SUCCESS;
+}
 int
 main(void)
 {
-	char lstr[5];
+	int ii, jj;
 
-        /* set global to 1 */
-	memset(&gstr,1,9*sizeof(char));
-	
-   	/* set local to 3 */
-	memset(&lstr,3,5*sizeof(char));
-
-	if ((gstr [7] + lstr[4]) != 4)
-	{
-		return EXIT_FAILURE;
-	}
+	for(ii=0;ii<sziibgn;ii++)
+		for(jj=0;jj<sziibgn;jj++){
+			if( testit(iibgn[ii],iibgn[jj]+1) == EXIT_FAILURE)
+			return EXIT_FAILURE;
+		}
 	return EXIT_SUCCESS;
 }

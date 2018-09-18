216e505ce4617ae593a0c04d1a1709b53226336c
David Young <dyoung@pobox.com>
2018-09-18 18:54:09 -0500

    Now I have found that I'm *still* not using the right objdirs when ctools/toolset/ builds its version of cross/{linux,qnx}/, however, cross/{linux,qnx}/ build fine under other circumstances.

--- cross-libdir.mk
+++ cross-libdir.mk
@@ -4,20 +4,33 @@ LIBDIR=$(PREFIX)/lib/qnx
 .endif
 
 .if $(DEBUG:Uno) == yes
-.info CROSS_OBJTOP=$(CROSS_OBJTOP)
-.info CROSS_SRCTOP=$(CROSS_SRCTOP)
-.info .CURDIR=$(.CURDIR)
-.info .OBJDIR=$(.OBJDIR)
-.info MAKEOBJDIR=$(MAKEOBJDIR)
-.info MKOBJDIRS=$(MKOBJDIRS)
-.info OS=$(OS)
-.info MAKEFLAGS=$(MAKEFLAGS)
+.info enter CROSS_OBJTOP=$(CROSS_OBJTOP)
+.info enter CROSS_SRCTOP=$(CROSS_SRCTOP)
+.info enter .CURDIR=$(.CURDIR)
+.info enter .OBJDIR=$(.OBJDIR)
+.info enter MAKEOBJDIR=$(MAKEOBJDIR)
+.info enter MKOBJDIRS=$(MKOBJDIRS)
+.info enter OS=$(OS)
+.info enter MAKEFLAGS=$(MAKEFLAGS)
+.info enter LIBDIR=$(LIBDIR)
 .endif
 
 .if defined(CROSS_OBJTOP)
-MAKEOBJDIR='$$(.CURDIR:C,${CROSS_SRCTOP},${CROSS_OBJTOP},)'
+MAKEOBJDIR=$(.CURDIR:C,${CROSS_SRCTOP},${CROSS_OBJTOP},)
 MAKEFLAGS+=	MKOBJDIRS=auto
 MAKEFLAGS+=	MAKEOBJDIR='$$(.CURDIR:C,${CROSS_SRCTOP},${CROSS_OBJTOP},)'
+.endif
+
+.if $(DEBUG:Uno) == yes
+.info exit CROSS_OBJTOP=$(CROSS_OBJTOP)
+.info exit CROSS_SRCTOP=$(CROSS_SRCTOP)
+.info exit .CURDIR=$(.CURDIR)
+.info exit .OBJDIR=$(.OBJDIR)
+.info exit MAKEOBJDIR=$(MAKEOBJDIR)
+.info exit MKOBJDIRS=$(MKOBJDIRS)
+.info exit OS=$(OS)
+.info exit MAKEFLAGS=$(MAKEFLAGS)
+.info exit LIBDIR=$(LIBDIR)
 .endif
 
 #.warning CROSS_SRCTOP=$(CROSS_SRCTOP) CROSS_OBJTOP=$(CROSS_OBJTOP) .OBJDIR=$(.OBJDIR) MAKEFLAGS=$(MAKEFLAGS)

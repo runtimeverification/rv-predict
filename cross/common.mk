bf141ef442509a4005144f19abd155fc5b493a60
David Young <dyoung@pobox.com>
2018-09-19 12:29:32 -0500

    Simplify.  And make it build.  At least, it built once, in my workspace. I am having trouble getting anything to reproduce, ugh.

--- cross/common.mk
+++ cross/common.mk
@@ -6,18 +6,4 @@ CROSS_OBJTOP:=${MAKEOBJDIR:U${.OBJDIR}}
 .export CROSS_OBJTOP
 .endif	# CROSS_SRCTOP unset
 
-.if 0
-MAKEFLAGS+=	MKOBJDIRS=auto
-MAKEFLAGS+=     MAKEOBJDIR='$$(.CURDIR:C,${CROSS_SRCTOP},${CROSS_OBJTOP},)'
-
-.if $(DEBUG:Uno) == yes
-.info CROSS_OBJTOP=$(CROSS_OBJTOP)
-.info CROSS_SRCTOP=$(CROSS_SRCTOP)
-.info .OBJDIR=$(.OBJDIR)
-.info MAKEOBJDIR=$(MAKEOBJDIR)
-.info OS=$(OS)
-.info MAKEFLAGS=$(MAKEFLAGS)
-.endif
-.endif # 0
-
 .include "${.PARSEDIR}/../cross-libdir.mk"

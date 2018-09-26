dd6ac16244171a7e55ddcd64c56b163b6c75d763
David Young <dyoung@pobox.com>
2018-09-26 16:41:07 -0500

    Use a cursor instead of a buf for __rvpredict_func_entry.  This definitely budges the performance profile in the right direction.

--- llvm/ngrt/func.c
+++ llvm/ngrt/func.c
@@ -10,13 +10,21 @@ __rvpredict_func_entry(const void *cfa, const void *callsite)
 		return __builtin_return_address(0);
 
 	rvp_ring_t *r = rvp_ring_for_curthr();
-	rvp_buf_t b = RVP_BUF_INITIALIZER;
 	const void *retaddr = __builtin_return_address(0);
 
+#if !defined(use_cursor)
+	rvp_buf_t b = RVP_BUF_INITIALIZER;
 	rvp_buf_put_pc_and_op(&b, &r->r_lastpc, retaddr, RVP_OP_ENTERFN);
 	rvp_buf_put_voidptr(&b, cfa);
 	rvp_buf_put_voidptr(&b, callsite);
 	rvp_ring_put_buf(r, b);
+#else
+	rvp_cursor_t c = rvp_cursor_for_ring(r);
+	rvp_cursor_put_pc_and_op(&c, &r->r_lastpc, retaddr, RVP_OP_ENTERFN);
+	rvp_cursor_put_voidptr(&c, cfa);
+	rvp_cursor_put_voidptr(&c, callsite);
+	rvp_ring_advance_to_cursor(r, &c);
+#endif
 
 	return retaddr;
 }

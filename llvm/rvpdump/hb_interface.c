d2e9cad1501d62765ff5c7eac1c9e1110a50102e
David Young <dyoung@pobox.com>
2018-11-02 15:27:38 -0500

    Add more example code.

--- llvm/rvpdump/hb_interface.c
+++ llvm/rvpdump/hb_interface.c
@@ -16,3 +16,18 @@ extern "C" void hb_perform_op(const rvp_pstate_t *ps, const rvp_ubuf_t *ub,
 }
 
 
+int
+main(void)
+{
+	const rvp_emitters_t hb_emitters = {
+		  .init = NULL
+		, .emit_nop = hb_perform_nop
+		, .emit_op = hb_perform_op
+		, .dataptr_to_string = NULL
+		, .insnptr_to_string = NULL
+	};
+
+	// 1 create your rvp_emitters_t emitters
+	// 2 call rvp_trace_dump_with_emitters(false, SIZE_MAX, &emitters,
+	//            input file descriptor);
+}

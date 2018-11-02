a7a3c9bd214f89efebfdffc53323833036818688
David Young <dyoung@pobox.com>
2018-11-02 15:04:13 -0500

    Changes designed to make reader.c & reader.h reusable from C++. There's still work to be done: need to extract `librvreader` for reuse by Umang's HB implementation in C++.

--- llvm/rvpdump/reader.c
+++ llvm/rvpdump/reader.c
@@ -13,7 +13,6 @@
 
 #include "io.h"	/* for __arraycount, offsetof */
 #include "nbcompat.h"	/* for __arraycount, offsetof */
-#include "tracefmt.h"
 #include "reader.h"
 
 #define	RSVD_IDEPTH 0
@@ -22,29 +21,6 @@ typedef char prebuf_t[
     sizeof("tid 4294967295.4294967295 18446744073709551615 ") +
     MAX(sizeof("{0x0123456789abcdef}"), sizeof("pc 0x0123456789abcdef"))];
 
-typedef union {
-	rvp_addr_t ub_pc;
-	rvp_begin_t ub_begin;
-	rvp_fork_join_switch_t ub_fork_join_switch;
-	rvp_load1_2_4_store1_2_4_t ub_load1_2_4_store1_2_4;
-	rvp_acquire_release_t ub_acquire_release;
-	rvp_load8_store8_t ub_load8_store8;
-	rvp_cog_t ub_cog;
-	rvp_entersig_t ub_entersig;
-	rvp_exitsig_t ub_exitsig;
-	rvp_sigdepth_t ub_sigdepth;
-	rvp_sigest_t ub_sigest;
-	rvp_sigdis_t ub_sigdis;
-	rvp_sigmaskmemo_t ub_sigmaskmemo;
-	rvp_sigmask_access_t ub_sigmask_access;
-	rvp_sigmask_rmw_t ub_sigmask_rmw;
-	rvp_rmw1_2_t ub_rmw1_2;
-	rvp_rmw4_t ub_rmw4;
-	rvp_rmw8_t ub_rmw8;
-	rvp_enterfn_t ub_enterfn;
-	char ub_bytes[4096];
-} rvp_ubuf_t;
-
 typedef struct {
 	size_t oi_reclen;
 	const char *oi_descr;
@@ -52,72 +28,6 @@ typedef struct {
 
 #define OP_INFO_INIT(__ty, __descr)	\
 	{.oi_reclen = sizeof(__ty), .oi_descr = __descr}
-
-typedef struct _rvp_frame {
-	rvp_addr_t	f_pc;	/* Program Counter where runtime logged
-				 * function entry
-				 */
-	rvp_addr_t	f_cfa;	/* DWARF Canonical Frame Address */
-} rvp_frame_t;
-
-typedef struct _rvp_call {
-	rvp_frame_t	*cs_frame;
-	int		cs_depth;
-	int		cs_nframes;
-} rvp_callstack_t;
-
-struct _rvp_pstate;
-typedef struct _rvp_pstate rvp_pstate_t;
-
-typedef struct _rvp_emitters {
-	void (*init)(const rvp_pstate_t *, const rvp_trace_header_t *);
-	void (*emit_nop)(const rvp_pstate_t *, const rvp_ubuf_t *);
-	void (*emit_op)(const rvp_pstate_t *, const rvp_ubuf_t *, rvp_op_t,
-	    bool, int);
-	char *(*dataptr_to_string)(const rvp_pstate_t *, char *, size_t,
-	    rvp_addr_t);
-	char *(*insnptr_to_string)(const rvp_pstate_t *, char *, size_t,
-	    rvp_addr_t);
-} rvp_emitters_t;
-
-#define	RVP_SIGNAL_DEPTH	16
-
-/* parse state: per-thread */
-typedef struct _rvp_thread_pstate {
-	bool		ts_present;
-	rvp_callstack_t	ts_callstack[RVP_SIGNAL_DEPTH];
-	rvp_addr_t	ts_lastpc[RVP_SIGNAL_DEPTH];
-	uint64_t	ts_generation[RVP_SIGNAL_DEPTH];
-	uint64_t	ts_nops[RVP_SIGNAL_DEPTH];
-	uint64_t	ts_last_gid[RVP_SIGNAL_DEPTH];
-	bool		ts_sigs_masked;
-} rvp_thread_pstate_t;
-
-/* parse state: global */
-struct _rvp_pstate {
-	rvp_thread_pstate_t	*ps_thread;
-	uint32_t		ps_nthreads;
-	rvp_addr_t		ps_deltop_first, ps_deltop_center,
-				ps_deltop_last;
-	uint32_t		ps_curthread;
-	const rvp_emitters_t	*ps_emitters;
-	uint32_t		ps_idepth;
-	int			ps_zeromasknum;
-	bool			ps_emit_generation;
-};
-
-typedef struct _intmax_item intmax_item_t;
-
-struct _intmax_item {
-	uintmax_t	i_v;
-	intmax_item_t *	i_next;
-	uint32_t	i_id;
-};
-
-typedef struct _intmax_bucket {
-	intmax_item_t *	b_head;
-	size_t		b_nitems;
-} intmax_bucket_t;
 
 static char *preamble_string(const rvp_pstate_t *, char *, size_t);
 
@@ -772,7 +682,7 @@ consume_and_print_trace(rvp_pstate_t *ps, rvp_ubuf_t *ub, size_t *nfullp)
 	}
 	extract_jmpvec_and_op_from_deltop(ps->ps_deltop_first,
 	    ub->ub_pc, &jmpvec, &op);
-	const op_info_t *oi = &op_to_info[op];
+	const op_info_t * const oi = &op_to_info[op];
 	if (oi->oi_descr == NULL)
 		errx(EXIT_FAILURE, "unknown op %d\n", op);
 	/* need to top off buffer? */

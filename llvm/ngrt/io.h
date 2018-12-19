390781e65f68dea551945b96af3ce53663ef9de3
David Young <dyoung@pobox.com>
2018-12-19 10:32:01 -0600

    Make sure readallv, writeall, and writeallv are treated as C symbols even by C++ programs.

--- llvm/ngrt/io.h
+++ llvm/ngrt/io.h
@@ -4,8 +4,14 @@
 #include <sys/uio.h>	/* for readv(2) */
 #include <sys/types.h>	/* for size_t and ssize_t */
 
+#include "nbcompat.h"	/* for __BEGIN_EXTERN_C, __END_EXTERN_C */
+
+__BEGIN_EXTERN_C
+
 ssize_t writeall(int, const void *, size_t);
 ssize_t readallv(int, const struct iovec *, struct iovec *, int);
 ssize_t writeallv(int, const struct iovec *, struct iovec *, int);
+
+__END_EXTERN_C
 
 #endif /* _RV_IO_H_ */
